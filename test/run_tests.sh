232a2e86b0931e0e717ab4528e8fc82f80d13062
lhinds <lhinds@redhat.com>
2019-01-07 09:58:05 +0000

    Implement pip requirements file for test
    This change adds a `test-requirements.yml` file to provide a
    central list of libaries required for testing keylime.
    
    Having a requirements file allows us to have some level of change
    control over library versions, see [0] for more information
    
    Closes # 20
    
    [0] https://pip.pypa.io/en/latest/user_guide/#requirements-files

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,20 +3,20 @@
 #
 # DISTRIBUTION STATEMENT A. Approved for public release: distribution unlimited.
 #
-# This material is based upon work supported by the Assistant Secretary of Defense for 
-# Research and Engineering under Air Force Contract No. FA8721-05-C-0002 and/or 
-# FA8702-15-D-0001. Any opinions, findings, conclusions or recommendations expressed in 
-# this material are those of the author(s) and do not necessarily reflect the views of the 
+# This material is based upon work supported by the Assistant Secretary of Defense for
+# Research and Engineering under Air Force Contract No. FA8721-05-C-0002 and/or
+# FA8702-15-D-0001. Any opinions, findings, conclusions or recommendations expressed in
+# this material are those of the author(s) and do not necessarily reflect the views of the
 # Assistant Secretary of Defense for Research and Engineering.
 #
 # Copyright 2017 Massachusetts Institute of Technology.
 #
 # The software/firmware is provided to you on an As-Is basis
 #
-# Delivered to the US Government with Unlimited Rights, as defined in DFARS Part 
-# 252.227-7013 or 7014 (Feb 2014). Notwithstanding any copyright notice, U.S. Government 
-# rights in this work are defined by DFARS 252.227-7013 or DFARS 252.227-7014 as detailed 
-# above. Use of this work other than as specifically authorized by the U.S. Government may 
+# Delivered to the US Government with Unlimited Rights, as defined in DFARS Part
+# 252.227-7013 or 7014 (Feb 2014). Notwithstanding any copyright notice, U.S. Government
+# rights in this work are defined by DFARS 252.227-7013 or DFARS 252.227-7014 as detailed
+# above. Use of this work other than as specifically authorized by the U.S. Government may
 # violate any copyrights that exist in this work.
 #
 ##########################################################################################
@@ -25,11 +25,11 @@
 # Find Keylime directory (we're in test/ directory)
 KEYLIME_DIR=`pwd`/../
 
-# Get list of tests in the test directory 
+# Get list of tests in the test directory
 TEST_LIST=`ls | grep "^test_.*\.py$"`
 
 
-# Command line params 
+# Command line params
 USER_MODE=0
 UMODE_OPT=""
 COVERAGE=0
@@ -37,18 +37,18 @@ COVERAGE_DIR=`pwd`
 unset COVERAGE_FILE
 while getopts ":chu" opt; do
     case $opt in
-        c) 
+        c)
             COVERAGE=1
             export COVERAGE_DIR=`mktemp -d`
             export COVERAGE_FILE=$COVERAGE_DIR/.coverage
             echo "INFO: Using Coverage directory: $COVERAGE_DIR"
             ;;
-        u)  
+        u)
             USER_MODE=1
             UMODE_OPT="--user"
             export KEYLIME_TEST=True
             ;;
-        h) 
+        h)
             echo "Usage: $0 [option...]"
             echo "Options:"
             echo $'-c \t\t Run Coverage scans'
@@ -60,7 +60,7 @@ while getopts ":chu" opt; do
 done
 
 
-# Permissions-related sanity checking 
+# Permissions-related sanity checking
 if [[ "$USER_MODE" -eq "0" && $EUID -ne 0 ]]; then
    echo "This script must be run as root in order to call apt-get and install python dependencies.  Use -u for user mode." 1>&2
    exit 1
@@ -72,7 +72,7 @@ if [[ "$USER_MODE" -eq "1" && $EUID -eq 0 ]]; then
 fi
 
 
-# Keylime directory validity check 
+# Keylime directory validity check
 if [[ ! -d "$KEYLIME_DIR/test" || ! -d "$KEYLIME_DIR/keylime" ]] ; then
     echo "ERROR: Invalid keylime directory at $KEYLIME_DIR"
     exit 1
@@ -80,8 +80,8 @@ fi
 
 
 # pip-related dependencies
-if [[ -z "$(command -v pip)" ]] ; then 
-    # Which package management system are we using? 
+if [[ -z "$(command -v pip)" ]] ; then
+    # Which package management system are we using?
     if [[ -n "$(command -v yum)" ]]; then
         PACKAGE_MGR=$(command -v yum)
         PYTHON_PREIN="epel-release python"
@@ -94,13 +94,13 @@ if [[ -z "$(command -v pip)" ]] ; then
        echo "No recognized package manager found on this system!" 1>&2
        exit 1
     fi
-    
-    if [[ "$USER_MODE" -ne "0" ]] ; then 
+
+    if [[ "$USER_MODE" -ne "0" ]] ; then
         echo -e "Packages '$PYTHON_DEPS' are required for this script.  Please manually install them, or run this script as root." 1>&2
         exit 1
     fi
-    
-    echo 
+
+    echo
     echo "=================================================================================="
     echo $'\t\t\tInstalling python and pip'
     echo "=================================================================================="
@@ -109,39 +109,29 @@ if [[ -z "$(command -v pip)" ]] ; then
 fi
 
 
-# Green-related dependencies
-if [[ -z "$(command -v green)" ]] ; then 
-    echo 
-    echo "=================================================================================="
-    echo $'\t\t\tInstalling python Green libs'
-    echo "=================================================================================="
-    pip install $UMODE_OPT green 
-fi
+# Install test dependencies
+echo
+echo "=================================================================================="
+echo $'\t\t\tInstalling test requirements'
+echo "=================================================================================="
+pip install $UMODE_OPT -r $KEYLIME_DIR/test/test-requirements.yml
+
 
 
 # Run the tests as necessary
 if [[ "$COVERAGE" -eq "1" ]] ; then
-    # Coverage-related dependencies
-    if [[ -z "$(command -v coverage)" ]] ; then 
-        echo 
-        echo "=================================================================================="
-        echo $'\t\t\tInstalling python Coverage libs'
-        echo "=================================================================================="
-        pip install $UMODE_OPT coverage 
-    fi
-    
-    # Coverage config file 
+    # Coverage config file
     echo -e "[run]\nsource = $KEYLIME_DIR/keylime\nomit = /usr/local/lib/,\"*/test/*\",$KEYLIME_DIR/keylime/test.py" > $COVERAGE_DIR/.coveragerc
-    
-    echo 
+
+    echo
     echo "=================================================================================="
     echo $'\t\t\tBegin Testing Phase'
     echo "=================================================================================="
     for test in $TEST_LIST ; do
         coverage run --rcfile=$COVERAGE_DIR/.coveragerc --parallel-mode $test
     done
-    
-    echo 
+
+    echo
     echo "=================================================================================="
     echo $'\t\t\tProcessing Coverage Reports'
     echo "=================================================================================="
@@ -150,9 +140,9 @@ if [[ "$COVERAGE" -eq "1" ]] ; then
     coverage report
     coverage html
     popd 1>/dev/null
-else 
-    # Do generic testing with no coverage 
-    echo 
+else
+    # Do generic testing with no coverage
+    echo
     echo "=================================================================================="
     echo $'\t\t\tBegin Testing Phase'
     echo "=================================================================================="
