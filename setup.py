81bdc5bd640f1300dac38f84bfebddfda0b06c73
Luke Hinds <lhinds@redhat.com>
2019-09-12 15:00:01 +0100

    Implements python-cryptography module
    This change removes pycryptodome and replaces it with the
    cryptography library.
    
    This is largely to do with the cryptography being a wrapper of
    openssl, which is under greater scrutiny for security bugs
    than pycryptodome.
    
    cryptography is also widely available as an OS package with all
    Linux distributions.
    
    Resolves: #127

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pycryptodomex>=3.4.1','tornado>=4.3,<6','m2crypto>=0.21.1','pyzmq>=14.4','pyyaml>=3.11','simplejson>=3.8','requests>=2.6'],
+    install_requires=['cryptography>=2.1.4','tornado>=4.3,<6','m2crypto>=0.21.1','pyzmq>=14.4','pyyaml>=3.11','simplejson>=3.8','requests>=2.6'],
 
     # test packages required
     tests_require=['green','coverage'],
