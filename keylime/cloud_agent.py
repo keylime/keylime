927631803ddd6714844d8ef9791955407186d076
Rajat <rb.macboy@gmail.com>
2019-12-20 09:22:25 +0530

    Revert "Added cloudagent_ip in INI config"
    This reverts commit d99fab41f4e99ae4d32a726ea59530e147fea47c.

--- keylime/cloud_agent.py
+++ keylime/cloud_agent.py
@@ -548,11 +548,11 @@ def main(argv=sys.argv):
     if not retval:
         raise Exception("Registration failed on activate")
 
-    serveraddr = (config.get('general', 'cloudagent_ip'), config.getint('general', 'cloudagent_port'))
+    serveraddr = ('', config.getint('general', 'cloudagent_port'))
     server = CloudAgentHTTPServer(serveraddr,Handler,agent_uuid)
     serverthread = threading.Thread(target=server.serve_forever)
 
-    logger.info(f"Starting Cloud Agent on {serveraddr[0]}:{serveraddr[1]} use <Ctrl-C> to stop")
+    logger.info( 'Starting Cloud Agent on port %s use <Ctrl-C> to stop'%serveraddr[1])
     serverthread.start()
 
     # want to listen for revocations?
