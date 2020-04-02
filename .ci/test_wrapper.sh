d84d9369caa265fe5885fe2ebe85560969c4d2d4
Luke Hinds <lhinds@redhat.com>
2020-04-02 15:18:54 +0100

    Fix merge conflict

--- .ci/test_wrapper.sh
+++ .ci/test_wrapper.sh
@@ -10,9 +10,6 @@ install -c tpm_serverd /usr/local/bin/tpm_serverd
 install -c init_tpm_server /usr/local/bin/init_tpm_server
 /usr/local/bin/tpm_serverd
 
-export TPM2TOOLS_TCTI="tabrmd:bus_name=com.intel.tss2.Tabrmd"
-pkill -HUP dbus-daemon
-
 # Run tests
 cd ${KEYLIME_HOME}/test
 chmod +x ./run_tests.sh
