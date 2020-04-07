e3e33faa4384ed02a1384eb26b1325f80da24f60
Luke Hinds <lhinds@redhat.com>
2020-04-07 14:38:31 +0100

    Fix CI run issues

--- .ci/test_wrapper.sh
+++ .ci/test_wrapper.sh
@@ -2,6 +2,14 @@
 
 KEYLIME_HOME=/root/keylime
 
+# Configure swtpm2
+cd ${KEYLIME_HOME}/swtpm2_scripts
+chmod +x init_tpm_server
+chmod +x tpm_serverd
+install -c tpm_serverd /usr/local/bin/tpm_serverd
+install -c init_tpm_server /usr/local/bin/init_tpm_server
+/usr/local/bin/tpm_serverd
+
 # Run tests
 cd ${KEYLIME_HOME}/test
 chmod +x ./run_tests.sh
