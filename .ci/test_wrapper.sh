a15ec0151dc701502dd067cf586d6bf5a78833ae
Luke Hinds <lhinds@redhat.com>
2020-04-09 16:12:21 +0100

    Revert CI Changes

--- .ci/test_wrapper.sh
+++ .ci/test_wrapper.sh
@@ -8,7 +8,17 @@ chmod +x init_tpm_server
 chmod +x tpm_serverd
 install -c tpm_serverd /usr/local/bin/tpm_serverd
 install -c init_tpm_server /usr/local/bin/init_tpm_server
+# Server needs to be running, or tpm2-abrmd.service will fail.
 /usr/local/bin/tpm_serverd
+
+export TPM2TOOLS_TCTI="tabrmd:bus_name=com.intel.tss2.Tabrmd"
+pkill -HUP dbus-daemon
+
+# Configure tpm2-abrmd systemd
+sed -i 's/.*ExecStart.*/ExecStart=\/usr\/sbin\/tpm2-abrmd --tcti=mssim/' /usr/lib/systemd/system/tpm2-abrmd.service
+systemctl daemon-reload
+systemctl enable tpm2-abrmd
+systemctl start tpm2-abrmd
 
 # Run tests
 cd ${KEYLIME_HOME}/test
