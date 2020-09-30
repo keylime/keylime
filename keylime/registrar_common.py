cfc1e95f1e8a08206687c06db372c52d579a96c5
Marcio Silva <marcios@us.ibm.com>
2020-09-30 17:13:00 -0400

    Requested changes

--- keylime/registrar_common.py
+++ keylime/registrar_common.py
@@ -391,10 +391,9 @@ class UnprotectedHandler(BaseHTTPRequestHandler, SessionManager):
                     except SQLAlchemyError as e:
                         logger.error(f'SQLAlchemy Error: {e}')
                 else:
-                    try :
+                    if drivername == "mysql" :
                         agent.key = agent.key.encode('utf-8')
-                    except :
-                        Pass
+
                     ex_mac = crypto.do_hmac(agent.key, agent_id)
                     if ex_mac == auth_tag:
                         try:
