17787e33b4f0fd8af7da953e327ab6db7a289456
Stefan Berger <stefanb@linux.vnet.ibm.com>
2020-12-19 09:07:54 -0500

    codestyle: Sort remaining disabled linters
    Sort the remaining disabled linters. The first line of disabled
    linters should be the easiest to activate.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- scripts/check_codestyle.sh
+++ scripts/check_codestyle.sh
@@ -6,12 +6,10 @@ if [ -z "$(type -P pylint)" ]; then
 fi
 
 pylint \
-  --disable C0103,C0115,C0116,C0200,C0301,C0302,C0411,C0114,C0111 \
-  --disable W0707,W0223,W1201,W0603,W0613,W0703,W0102 \
-  --disable W1505,W0511,W1509 \
-  --disable W1203 \
+  --disable C0200,C0411,C0114,W0707,W0223,W0613,W1505,W1509 \
+  --disable C0103,C0115,C0116,C0301,C0302,C0111 \
+  --disable W0102,W0511,W0603,W0703,W1201,W1203 \
   --disable E0401,E1101,E1120 \
-  --disable R0801,R0902,R0903,R0912,R0914,R0915,R0201,R0911 \
-  --disable R0913,R0904 \
+  --disable R0801,R0902,R0903,R0904,R0912,R0913,R0914,R0915,R0201,R0911 \
   *.py $(find ./keylime ./test -name '*.py' ! -name 'oldtest.py')
 exit $?
