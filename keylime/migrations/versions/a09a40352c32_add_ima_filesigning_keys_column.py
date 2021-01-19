6a2d5ef4a945af0f5fdd6dea0fa7f7fed05c0ad3
Kaifeng Wang <kaifeng.w@gmail.com>
2021-01-19 10:05:49 +0800

    Fix brached migration path
    There were two database schema changes merged but not dependent on each
    other, so current migration path is branched.
    
    Update dependency according to merge sequence.
    
    Signed-off-by: Kaifeng Wang <kaifeng.w@gmail.com>

--- keylime/migrations/versions/a09a40352c32_add_ima_filesigning_keys_column.py
+++ keylime/migrations/versions/a09a40352c32_add_ima_filesigning_keys_column.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'a09a40352c32'
-down_revision = 'eeb702f77d7d'
+down_revision = '8da20383f6e1'
 branch_labels = None
 depends_on = None
 
