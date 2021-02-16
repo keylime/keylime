b73cc8fe8b90d2d86e2281d6f08091a90cabb806
Nicolas Oliver <nicolasoliver03@gmail.com>
2021-02-16 16:12:21 -0300

    Write boot_aggregate in allowlist
    Signed-off-by: Nicolas Oliver <nicolasoliver03@gmail.com>

--- scripts/create_allowlist.sh
+++ scripts/create_allowlist.sh
@@ -60,6 +60,13 @@ rm -f $OUTPUT
 
 echo "Writing allowlist to $OUTPUT with $ALGO..."
 
+# Add boot_aggregate from /sys/kernel/security/ima/ascii_runtime_measurements (IMA Log) file.
+# The boot_aggregate measurement is always the first line in the IMA Log file.
+# The format of the log lines is the following:
+#     <PCR_ID> <PCR_Value> <IMA_Template> <File_Digest> <File_Name> <File_Signature>
+# File_Digest may start with the digest algorithm specified (e.g "sha1:", "sha256:") depending on the template used.
+head -n 1 /sys/kernel/security/ima/ascii_runtime_measurements | awk '{ print $4 "  boot_aggregate" }' | sed 's/.*://' >> $OUTPUT
+
 # Add all appropriate files under root FS to allowlist
 cd /
 find `ls / | grep -v "\bsys\b\|\brun\b\|\bproc\b\|\blost+found\b\|\bdev\b\|\bmedia\b\|\bsnap\b\|mnt"` \( -fstype rootfs -o -xtype f -type l -o -type f \) -uid 0 -exec $ALGO '/{}' >> $OUTPUT \;
