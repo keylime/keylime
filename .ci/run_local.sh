42f2341a344c46de4af9c54f090ea0fbf6caaac6
Stefan Berger <stefanb@linux.ibm.com>
2021-03-22 11:31:55 -0400

    CI/CD: Remove tpm1.2 testing support
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- .ci/run_local.sh
+++ .ci/run_local.sh
@@ -6,26 +6,12 @@
 REPO=${KEYLIME_REPO_PATH:-${1:-/home/${USER}/keylime}}
 
 # keylime images
-tpm12image="lukehinds/keylime-ci-tpm12"
-tpm12tag="v500"
 tpm20image="lukehinds/keylime-ci-tpm20"
 tpm20tag="v101"
 
 echo -e "Grabbing latest images"
 
-docker pull ${tpm12image}:${tpm12tag}
 docker pull ${tpm20image}:${tpm20tag}
-
-function tpm1 {
-    container_id=$(mktemp)
-    docker run --detach --privileged \
-        -v $REPO:/root/keylime \
-        -it ${tpm12image}:${tpm12tag} >> ${container_id}
-    docker exec -u 0 -it --tty "$(cat ${container_id})" \
-        /bin/sh -c 'cd /root/keylime/test; chmod +x ./run_tests.sh; ./run_tests.sh -s openssl'
-    docker stop "$(cat ${container_id})"
-    docker rm "$(cat ${container_id})"
-}
 
 function tpm2 {
     container_id=$(mktemp)
@@ -41,11 +27,10 @@ function tpm2 {
 
 while true; do
     echo -e ""
-    read -p "Do you wish to test against TPM1.2(a) / TPM 2.0(b) or q(quit): " abq
+    read -p "Do you wish to test against TPM 2.0(a) or q(quit): " abq
     case $abq in
-        [a]* ) tpm1;;
-        [b]* ) tpm2;;
+        [a]* ) tpm2;;
         [q]* ) exit;;
-        * ) echo "Please answer 1, 2 q(quit)";;
+        * ) echo "Please answer a or q(quit)";;
     esac
 done
