cd344a23589b6196efc9d59bc285508b2b04fb10
Stefan Berger <stefanb@linux.ibm.com>
2021-03-22 12:01:42 -0400

    run_local.sh: Run unit tests in keylime/tpm/tpm2_objects.py
    When running local tests using run_local.sh the unit tests in
    tpm2_object.py did not run since we were in the wrong directory then.
    This patch fixes this problem.
    
    python3: can't open file 'keylime/tpm/tpm2_objects.py': [Errno 2] No such file or directory
    
    Also set the executable flag on run_local.sh so that we can run it like
    this for example:
    
    ./run_local.sh ${PWD}/../
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -11,7 +11,13 @@ alias python='/usr/bin/python3'
 KEYLIME_DIR=$(realpath "$(dirname "$(readlink -f "$0")")/../")
 
 # Run separate unit tests
+pushd "${KEYLIME_DIR}"
 python3 keylime/tpm/tpm2_objects.py
+if [ $? -ne 0 ]; then
+	echo "Error: tpm2_objects.py unit tests failed."
+	exit 1
+fi
+popd
 
 # Get list of tests in the test directory
 TEST_LIST=`ls | grep "^test_.*\.py$"`
