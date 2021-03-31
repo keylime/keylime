keylime/keylime

commit 03132e1d13d88a84961b6814294bd8d938085e06

codestyle: Fix issues related to W0621
This patch fixes the following issues detected by pylint:

************* Module keylime.registrar_common
keylime/registrar_common.py:481:23: W0621: Redefining name 'signal' from outer scope (line 8) (redefined-outer-name)
************* Module keylime.tornado_requests
keylime/tornado_requests.py:31:8: W0621: Redefining name 'request' from outer scope (line 18) (redefined-outer-name)
************* Module keylime.tenant_webapp
keylime/tenant_webapp.py:634:4: W0621: Redefining name 'my_cert' from outer scope (line 28) (redefined-outer-name)
keylime/tenant_webapp.py:635:4: W0621: Redefining name 'my_priv_key' from outer scope (line 28) (redefined-outer-name)
************* Module keylime.tpm_ek_ca
keylime/tpm_ek_ca.py:34:4: W0621: Redefining name 'trusted_certs' from outer scope (line 14) (redefined-outer-name)
************* Module test.test_restful
test/test_restful.py:194:4: W0621: Redefining name 'my_cert' from outer scope (line 109) (redefined-outer-name)
test/test_restful.py:194:13: W0621: Redefining name 'my_priv_key' from outer scope (line 110) (redefined-outer-name)

Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

keylime/tenant_webapp.py
@@ -631,15 +631,15 @@ def start_tornado(tornado_server, port):
 
 def get_tls_context():
     ca_cert = config.get('tenant', 'ca_cert')
-    my_cert = config.get('tenant', 'my_cert')
-    my_priv_key = config.get('tenant', 'private_key')
+    my_tenant_cert = config.get('tenant', 'my_cert')
+    my_tenant_priv_key = config.get('tenant', 'private_key')
 
     tls_dir = config.get('tenant', 'tls_dir')
 
     if tls_dir == 'default':
         ca_cert = 'cacert.crt'
-        my_cert = 'client-cert.crt'
-        my_priv_key = 'client-private.pem'
+        my_tenant_cert = 'client-cert.crt'
+        my_tenant_priv_key = 'client-private.pem'
         tls_dir = 'cv_ca'
 
     # this is relative path, convert to absolute in WORK_DIR
@@ -649,13 +649,13 @@ def get_tls_context():
     logger.info(f"Setting up client TLS in {tls_dir}")
 
     ca_path = "%s/%s" % (tls_dir, ca_cert)
-    my_cert = "%s/%s" % (tls_dir, my_cert)
-    my_priv_key = "%s/%s" % (tls_dir, my_priv_key)
+    my_tls_cert = "%s/%s" % (tls_dir, my_cert)
+    my_tls_priv_key = "%s/%s" % (tls_dir, my_priv_key)
 
     context = ssl.create_default_context()
     context.load_verify_locations(cafile=ca_path)
     context.load_cert_chain(
-        certfile=my_cert, keyfile=my_priv_key)
+        certfile=my_tls_cert, keyfile=my_tls_priv_key)
     context.verify_mode = ssl.CERT_REQUIRED
     context.check_hostname = config.getboolean(
         'general', 'tls_check_hostnames')

keylime/registrar_common.py
@@ -478,7 +478,7 @@ def start(host, tlsport, port):
     for thread in threads:
         thread.start()
 
-    def signal_handler(signal, frame):
+    def signal_handler(signum, frame):
         do_shutdown(servers)
         sys.exit(0)
 

keylime/tornado_requests.py
@@ -28,12 +28,12 @@ async def request(method, url, params=None, data=None, context=None, headers=Non
         url = url.replace('http://', 'https://', 1)
 
     try:
-        request = httpclient.HTTPRequest(url=url,
-                                         method=method,
-                                         ssl_options=context,
-                                         body=data,
-                                         headers=headers)
-        response = await http_client.fetch(request)
+        req = httpclient.HTTPRequest(url=url,
+                                     method=method,
+                                     ssl_options=context,
+                                     body=data,
+                                     headers=headers)
+        response = await http_client.fetch(req)
 
     except httpclient.HTTPError as e:
         if e.response is None:

keylime/tpm_ek_ca.py
@@ -31,11 +31,11 @@ def check_tpm_cert_store():
 
 def cert_loader():
     file_list = glob.glob(os.path.join(os.getcwd(), tpm_cert_store + "*.pem"))
-    trusted_certs = []
+    my_trusted_certs = []
     for file_path in file_list:
         with open(file_path) as f_input:
-            trusted_certs.append(f_input.read())
-    return trusted_certs
+            my_trusted_certs.append(f_input.read())
+    return my_trusted_certs
 
 
 atmel_trusted_keys = {"ATM1": {"key": "E5AFB8C97B6E6D49C624FCFB6558D6B291A17B94EBF4186B590F5039F0E85659268BEB60D86A9CF6A03D477F5686288498AB90DFEE1722F3BE5C516B542BFEF18B849C1208BB8E85117D4B046ECB9D425B920015D29CC73163A084B84320670683FEA43BD7C7FBB29CC1FC0D6A1F992E47DB7E2B37726D3D465386F114AAF0BC8AF19734ED01F633B2FFF0F1674514393270205BBBEE93C87F33AD934F2FB20D5B9697F8E7877BEB2A054CA0EC8BFE233711D147215FABFE524E23F1D1025922782446889D64248E2D42951788BFAD2B6881D720319EA712433596C6536EBBAB00515DAD23EA8C54CDA8E9E4F57FF30CEFE6F89E6CAE06B74E20EE5DBD37A4EF",

scripts/check_codestyle.sh
@@ -8,10 +8,10 @@ fi
 pylint \
   --disable C0103,C0115,C0116,C0200,C0301,C0302,C0411,C0114,C0111 \
   --disable W0707,W0223,W1201,W0603,W0613,W0703,W0102 \
-  --disable W1505,W0511,W0621,W0612,W1509 \
+  --disable W1505,W0511,W0612,W1509 \
   --disable W1203 \
   --disable E0401,E1101,E1120 \
   --disable R0801,R0902,R0903,R0912,R0914,R0915,R0201,R0911 \
   --disable R0913,R0904 \
-  *.py $(find ./keylime ./test -name '*.py')
+  *.py $(find ./keylime ./test -name '*.py' ! -name 'oldtest.py')
 exit $?

test/test_restful.py
@@ -191,8 +191,8 @@ def setUpModule():
     tenant_templ.registrar_base_tls_url = f'{tenant_templ.registrar_ip}:{tenant_templ.registrar_tls_boot_port}'
     tenant_templ.agent_base_url = f'{tenant_templ.cloudagent_ip}:{tenant_templ.cloudagent_port}'
     # Set up TLS
-    my_cert, my_priv_key = tenant_templ.get_tls_context()
-    tenant_templ.cert = (my_cert, my_priv_key)
+    my_tls_cert, my_tls_priv_key = tenant_templ.get_tls_context()
+    tenant_templ.cert = (my_tls_cert, my_tls_priv_key)
 
 
 # Destroy everything on teardown

commit df9665eb8ee9633394628843b44ba8d1a3f8e887

webapp: Fix private key and certificate path (issue #604)
03132e1d13d introduced a bug when renaming variables that were
overlapping with the outer scope and c775b24dbe removed the then
unused variables. This patch partially reverts those changes and
fixes the path for the private key and certificate.

This patch fixes issue #604.

Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

keylime/tenant_webapp.py
@@ -655,11 +655,15 @@ def start_tornado(tornado_server, port):
 
 def get_tls_context():
     ca_cert = config.get('tenant', 'ca_cert')
+    my_tenant_cert = config.get('tenant', 'my_cert')
+    my_tenant_priv_key = config.get('tenant', 'private_key')
 
     tls_dir = config.get('tenant', 'tls_dir')
 
     if tls_dir == 'default':
         ca_cert = 'cacert.crt'
+        my_tenant_cert = 'client-cert.crt'
+        my_tenant_priv_key = 'client-private.pem'
         tls_dir = 'cv_ca'
 
     # this is relative path, convert to absolute in WORK_DIR
@@ -669,8 +673,8 @@ def get_tls_context():
     logger.info("Setting up client TLS in %s", tls_dir)
 
     ca_path = "%s/%s" % (tls_dir, ca_cert)
-    my_tls_cert = "%s/%s" % (tls_dir, my_cert)
-    my_tls_priv_key = "%s/%s" % (tls_dir, my_priv_key)
+    my_tls_cert = "%s/%s" % (tls_dir, my_tenant_cert)
+    my_tls_priv_key = "%s/%s" % (tls_dir, my_tenant_priv_key)
 
     context = ssl.create_default_context()
     context.load_verify_locations(cafile=ca_path)