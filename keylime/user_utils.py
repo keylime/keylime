6012e498a7597d42669a894b2e87331e9858a534
Stefan Berger <stefanb@linux.ibm.com>
2021-04-12 09:23:53 -0400

    agent: Drop privileges by switching to normal user and group
    Extend the agent section in keylime.conf with a runas parameter where
    one can specify the user and group to run the agent as.
    
    If the agent was started as root check whether run_as is specified and
    if this is the case change the onwership on the tmpfs to the run_as
    user and after that switch to the user.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ keylime/user_utils.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+
+'''
+SPDX-License-Identifier: Apache-2.0
+Copyright 2022 IBM Corporation
+'''
+
+import os
+import grp
+import pwd
+
+from keylime import keylime_logging
+
+# Configure logger
+logger = keylime_logging.init_logging('privileges')
+
+
+def string_to_uidgid(user_and_group):
+    """ Translate the user_and_group string to uid and gid.
+        The userandgroup parameter must be a string of the format '<user>[:[<group>]]'
+        or '[<user>]:gid'
+        User and group can be strings or integers. If no group is given, -1 will be
+        returned for gid.
+        :raises ValueError: if user or group could not be resolved
+    """
+    params = user_and_group.split(':')
+    if len(params) > 2:
+        raise ValueError(f"User and group {user_and_group} are in wrong format. Expected <user>[:[<group>]] or [<user>]:group")
+
+    gid = None
+    if len(params) == 2 and len(params[1]) > 0:
+        if params[1].isnumeric():
+            gid = int(params[1])
+            if gid < 0:
+                raise ValueError(f"User and group {user_and_group} contains an illegal value")
+        else:
+            try:
+                gr = grp.getgrnam(params[1])
+                gid = gr.gr_gid
+            except KeyError as e:
+                raise ValueError(f'Could not resolve group {params[1]}: {e}') from e
+
+    uid = None
+    if len(params[0]) > 0:
+        if params[0].isnumeric():
+            uid = int(params[0])
+            if uid < 0:
+                raise ValueError(f"User and group {user_and_group} contains an illegal value")
+        else:
+            try:
+                passwd = pwd.getpwnam(params[0])
+                uid = passwd.pw_uid
+            except KeyError as e:
+                raise ValueError(f'Could not resolve user {params[0]}: {e}') from e
+
+    if uid is None and gid is None:
+        raise ValueError(f"User and group {user_and_group} are in wrong format. Expected <user>[:[<group>]] or [<user>]:group")
+
+    return uid, gid
+
+
+def change_uidgid(user_and_group):
+    """ Change uid and gid of the current process.
+        The user_and_group parameter must be a string of the format
+        '<user>[:[<group>]]' or [<user>]:<group>. User and group can
+        be strings or integers.
+        :raises ValueError: if user or group could not be resolved
+        :raises RuntimeError: if setting gid or uid did not succeed
+    """
+    uid, gid = string_to_uidgid(user_and_group)
+
+    # First change group and then user
+    if gid is not None:
+        try:
+            os.setgid(gid)
+        except (OSError, PermissionError) as e:
+            raise RuntimeError(f'Could not set gid to {gid}: {e}') from e
+
+    if uid is not None:
+        try:
+            os.setuid(uid)
+        except (OSError, PermissionError) as e:
+            raise RuntimeError(f'Could not set uid to {uid}: {e}') from e
+
+
+def chown(path, user_and_group):
+    """ Change ownership on a given file to the new owner described in
+        user_and_group string.
+        The user_and_group parameter must be a string of the format
+        '<user>[:[<group>]]' or [<user>]:<group>. User and group can
+        be strings or integers.
+        :raises ValueError: if user or group could not be resolved
+    """
+    uid, gid = string_to_uidgid(user_and_group)
+    if uid is None:
+        uid = -1
+    if gid is None:
+        gid = -1
+
+    os.chown(path, uid, gid)
---
+++ keylime/user_utils_test.py
@@ -0,0 +1,52 @@
+'''
+SPDX-License-Identifier: Apache-2.0
+Copyright 2022 IBM Corporation
+'''
+
+import unittest
+
+from keylime import user_utils
+
+class TestUserUtils(unittest.TestCase):
+
+    def test_string_to_uidgid(self):
+        uid, gid = user_utils.string_to_uidgid('root')
+        self.assertTrue(isinstance(uid, int))
+        self.assertTrue(gid is None)
+
+        uid, gid = user_utils.string_to_uidgid('root:')
+        self.assertTrue(isinstance(uid, int))
+        self.assertTrue(gid is None)
+
+        uid, gid = user_utils.string_to_uidgid(':root')
+        self.assertTrue(uid is None)
+        self.assertTrue(isinstance(gid, int))
+
+        uid, gid = user_utils.string_to_uidgid('root:root')
+        self.assertTrue(isinstance(uid, int))
+        self.assertTrue(isinstance(gid, int))
+
+        with self.assertRaises(ValueError):
+            uid, gid = user_utils.string_to_uidgid(':')
+
+        uid, gid = user_utils.string_to_uidgid('100')
+        self.assertTrue(uid == 100)
+        self.assertTrue(gid is None)
+
+        uid, gid = user_utils.string_to_uidgid('100:')
+        self.assertTrue(uid == 100)
+        self.assertTrue(gid is None)
+
+        uid, gid = user_utils.string_to_uidgid(':200')
+        self.assertTrue(uid is None)
+        self.assertTrue(gid == 200)
+
+        uid, gid = user_utils.string_to_uidgid('100:200')
+        self.assertTrue(uid == 100)
+        self.assertTrue(gid == 200)
+
+        with self.assertRaises(ValueError):
+            uid, gid = user_utils.string_to_uidgid('-100:200')
+
+        with self.assertRaises(ValueError):
+            uid, gid = user_utils.string_to_uidgid('nobodyever')
