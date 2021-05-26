f884d4ce7dd24ab3711d78fa4032f4d2552b185e
Marcio Silva <marcio.a.silva@ibm.com>
2021-05-26 16:25:58 -0400

    Flush all sessions from TPM device
    This commit is a quick (and admittedly ugly) hack to fix a problem that
    has been affecting our production environment. While it partially
    addresses #656, we have laid out a 3-phase plan to deal with the problem
    in a more consistent and sustainable manner.
    
    Signed-off-by: Marcio Silva <marcio.a.silva@ibm.com>

--- keylime/tpm/tpm_main.py
+++ keylime/tpm/tpm_main.py
@@ -505,6 +505,12 @@ class tpm(tpm_abstract.AbstractTPM):
             logger.info("Taking ownership with config provided TPM owner password")
             owner_pw = config_pw
 
+        logger.debug("Removing all saved sessions from TPM")
+        if self.tools_version == "3.2":
+            retDict = self.__run(["tpm2_flushcontext", "-s"], raiseOnError=False)
+        elif self.tools_version in ["4.0", "4.2"]:
+            retDict = self.__run(["tpm2_flushcontext", "-s"], raiseOnError=False)
+
         if self.tools_version == "3.2":
             retDict = self.__run(["tpm2_takeownership", "-c"], raiseOnError=False)
             retDict = self.__run(["tpm2_takeownership", "-o", owner_pw, "-e", owner_pw],
