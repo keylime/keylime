21085887c2004d17fd80742251b4e1d53ca9e08f
Thore Sommer <mail@thson.de>
2021-06-14 19:29:16 +0200

    ima: Use ima_ast for parsing and validation
    Use the new introduced parser and validator capabilities to handle IMA
    validation.
    
    This moves the parsing of the IMA entries out of the process_measurement_list
    function and validators unified where possible.
    
    All unused code and comments related to parsing are removed.
    
    Signed-off-by: Thore Sommer <mail@thson.de>

--- keylime/ima.py
+++ keylime/ima.py
@@ -11,192 +11,87 @@ import os
 import re
 import json
 import datetime
+import functools
 
 from keylime import config
 from keylime import gpg
 from keylime import keylime_logging
+from keylime import ima_ast
+
 
 logger = keylime_logging.init_logging('ima')
 
-#         m = ima_measure_re.match(measure_line)
-#         measure  = m.group('file_hash')
-#         filename = m.group('file_path')
-
-START_HASH = (codecs.decode('0000000000000000000000000000000000000000', 'hex'))
-FF_HASH = (codecs.decode('ffffffffffffffffffffffffffffffffffffffff', 'hex'))
 
 # The version of the allowlist format that is supported by this keylime release
 ALLOWLIST_CURRENT_VERSION = 1
-
-
-# struct event {
-#     struct {
-#         u_int32_t pcr;
-#         u_int8_t digest[SHA_DIGEST_LENGTH];
-#         u_int32_t name_len;
-#     } header;
-#     char name[TCG_EVENT_NAME_LEN_MAX + 1];
-#     struct ima_template_desc *template_desc; /* template descriptor */
-#     u_int32_t template_data_len;
-#     u_int8_t *template_data;    /* template related data */
-# };
 
 
 def read_unpack(fd, fmt):
     return struct.unpack(fmt, fd.read(struct.calcsize(fmt)))
 
 
-TCG_EVENT_NAME_LEN_MAX = 255
-SHA_DIGEST_LEN = 20
+def _validate_ima_ng(exclude_regex, allowlist, digest: ima_ast.Digest, path: ima_ast.Name):
+    if allowlist is not None:
+        if exclude_regex is not None and exclude_regex.match(path.name):
+            logger.debug("IMA: ignoring excluded path %s" % path)
+            return True
 
-defined_templates = {
-    'ima': 'd|n',
-    'ima-ng': 'd-ng|n-ng',
-    'ima-sig': 'd-ng|n-ng|sig;',
-}
+        accept_list = allowlist.get(path.name, None)
+        if accept_list is None:
+            logger.warning("File not found in allowlist: %s" % (path.name))
+            return False
 
+        if codecs.encode(digest.hash, 'hex').decode('utf-8') not in accept_list:
+            logger.warning("Hashes for file %s don't match %s not in %s" %
+                           (path.name,
+                            codecs.encode(digest.hash, 'hex').decode('utf-8'),
+                            accept_list))
+            return False
 
-def ima_eventname_parse():
-    pass
-
-
-def ima_eventdigest_ng_parse():
-    pass
-
-
-def ima_eventname_ng_parse():
-    pass
-
-
-def ima_eventsig_parse():
-    pass
+    return True
 
 
-supported_fields = {
-    "n": ima_eventname_parse,
-    "d-ng": ima_eventdigest_ng_parse,
-    "n-ng": ima_eventname_ng_parse,
-    "sig": ima_eventsig_parse,
-}
+def _validate_ima_sig(exclude_regex, ima_keyring, allowlist, digest: ima_ast.Digest, path: ima_ast.Name,
+                      signature: ima_ast.Signature):
+    valid_signature = False
+    if ima_keyring and signature:
 
+        if exclude_regex is not None and exclude_regex.match(path):
+            logger.debug(f"IMA: ignoring excluded path {path.name}")
+            return True
 
-def _extract_from_ima(tokens, template_hash):
-    """ Extract filedata_hash & path form 'ima' entry; return 1 in case error occurred """
-    filedata_hash = codecs.decode(tokens[3], "hex")
-    path = str(tokens[4])
+        if not ima_keyring.integrity_digsig_verify(signature.data, digest.hash, digest.algorithm):
+            logger.warning(f"signature for file {path.name} is not valid")
+            return False
 
-    # this is some IMA weirdness
-    if template_hash == START_HASH:
-        return filedata_hash, path, 0
+        valid_signature = True
+        logger.debug("signature for file %s is good" % path)
 
-    # verify template hash. yep this is terrible
-    # name needs to be null padded out to MAX len. +1 is for the null terminator of the string itself
-    fmt = "<%ds%ds%ds" % (len(filedata_hash), len(path), TCG_EVENT_NAME_LEN_MAX - len(path) + 1)
-    tohash = struct.pack(fmt,
-                         filedata_hash, path.encode("utf-8"),
-                         bytearray(TCG_EVENT_NAME_LEN_MAX - len(path) + 1))
-    expected_template_hash = hashlib.sha1(tohash).digest()
+    # If there is also a allowlist verify the file against that.
+    # This happens in the case that no signature or keyring this given or the signature is valid.
+    if allowlist is not None:
+        # We use the normal ima_ng validator to validate hash
+        return _validate_ima_ng(exclude_regex, allowlist, digest, path)
 
-    if expected_template_hash != template_hash:
-        error = 1
-        logger.warning("template hash for file %s does not match %s != %s" %
-                       (path,
-                        codecs.encode(expected_template_hash, 'hex').decode('utf-8'),
-                        codecs.encode(template_hash, 'hex').decode('utf-8')))
-    else:
-        error = 0
+    # If we don't have a allowlist and don't have a keyring we just ignore the validation.
+    if ima_keyring is None:
+        return True
 
-    return filedata_hash, path, error
-
-
-def _extract_from_ima_ng(tokens, template_hash):
-    """ Extract filedata_hash & path form 'ima-ng' entry; return 1 in case error occurred """
-    filedata = tokens[3]
-    ftokens = filedata.split(":")
-    filedata_hash = codecs.decode(ftokens[1], 'hex')
-    path = str(tokens[4])
-
-    # this is some IMA weirdness
-    if template_hash == START_HASH:
-        return filedata_hash, path, 0
-
-    filedata_algo = str(ftokens[0])
-    # verify template hash. yep this is terrible
-    fmt = "<I%dsBB%dsI%dsB" % (len(filedata_algo), len(filedata_hash), len(path))
-    # +2 for the : and the null terminator, and +1 on path for null terminator
-    tohash = struct.pack(fmt,
-                         len(filedata_hash) + len(filedata_algo) + 2,
-                         filedata_algo.encode('utf-8'), ord(':'), ord('\0'),
-                         filedata_hash,
-                         len(path) + 1,
-                         path.encode("utf-8"),
-                         ord('\0'))
-    expected_template_hash = hashlib.sha1(tohash).digest()
-
-    if expected_template_hash != template_hash:
-        error = 1
-        logger.warning("template hash for file %s does not match %s != %s" %
-                       (path,
-                        codecs.encode(expected_template_hash, 'hex').decode('utf-8'),
-                        codecs.encode(template_hash, 'hex').decode('utf-8')))
-    else:
-        error = 0
-
-    return filedata_hash, path, error
-
-
-def _extract_from_ima_sig(tokens, template_hash):
-    """ Extract filedata_hash & path form 'ima-sig' entry; return 1 in case error occurred """
-    filedata = tokens[3]
-    ftokens = filedata.split(":")
-    filedata_hash = codecs.decode(ftokens[1], 'hex')
-    path = str(tokens[4])
-
-    # this is some IMA weirdness
-    if template_hash == START_HASH:
-        return filedata_hash, path, None, None, 0
-
-    filedata_algo = str(ftokens[0])
-    # verify template hash. yep this is terrible
-    fmt = "<I%dsBB%dsI%dsB" % (len(filedata_algo), len(filedata_hash), len(path))
-    # +2 for the : and the null terminator, and +1 on path for null terminator
-    tohash = struct.pack(fmt,
-                         len(filedata_hash) + len(filedata_algo) + 2,
-                         filedata_algo.encode('utf-8'), ord(':'), ord('\0'),
-                         filedata_hash,
-                         len(path) + 1,
-                         path.encode("utf-8"),
-                         ord('\0'))
-    signature = b''
-    if len(tokens) == 6:
-        signature = codecs.decode(tokens[5], 'hex')
-    tohash += struct.pack("<I%ds" % len(signature), len(signature), signature)
-    expected_template_hash = hashlib.sha1(tohash).digest()
-
-    if expected_template_hash != template_hash:
-        error = 1
-        logger.warning("template hash for file %s does not match %s != %s" %
-                       (path,
-                        codecs.encode(expected_template_hash, 'hex').decode('utf-8'),
-                        codecs.encode(template_hash, 'hex').decode('utf-8')))
-    else:
-        error = 0
-
-    return filedata_hash, path, signature, filedata_algo, error
+    return valid_signature
 
 
 def process_measurement_list(lines, lists=None, m2w=None, pcrval=None, ima_keyring=None):
-    errs = [0, 0, 0, 0, 0]
-    runninghash = START_HASH
+    running_hash = ima_ast.START_HASH
     found_pcr = (pcrval is None)
+    errors = {}
 
     if lists is not None:
         if isinstance(lists, str):
             lists = ast.literal_eval(lists)
-        allowlist = lists['allowlist']
+        allow_list = lists['allowlist']
         exclude_list = lists['exclude']
     else:
-        allowlist = None
+        allow_list = None
         exclude_list = None
 
     is_valid, compiled_regex, err_msg = config.valid_exclude_list(exclude_list)
@@ -206,118 +101,52 @@ def process_measurement_list(lines, lists=None, m2w=None, pcrval=None, ima_keyri
         err_msg += " Exclude list will be ignored."
         logger.error(err_msg)
 
+    ima_validator = ima_ast.Validator(
+        {ima_ast.ImaSig: functools.partial(_validate_ima_sig, compiled_regex, ima_keyring, allow_list),
+         ima_ast.ImaNg: functools.partial(_validate_ima_ng, compiled_regex, allow_list),
+         ima_ast.Ima: functools.partial(_validate_ima_ng, compiled_regex, allow_list)
+         }
+    )
+
     for line in lines:
         line = line.strip()
         if line == '':
             continue
 
-        tokens = line.split(None, 5)
-        if len(tokens) < 5 or len(tokens) > 6:
-            logger.error("invalid measurement list file line: -%s-" % (line))
-            return None
+        try:
+            entry = ima_ast.Entry(line, ima_validator)
 
-        # print tokens
-        # pcr = tokens[0]
-        template_hash = codecs.decode(tokens[1], 'hex')
-        mode = tokens[2]
-        signature = None
-        filedata_algo = None
+            # update hash
+            running_hash = hashlib.sha1(running_hash + entry.template_hash).digest()
 
-        if mode == "ima-ng":
-            filedata_hash, path, error = _extract_from_ima_ng(tokens,
-                                                              template_hash)
-        elif mode == "ima-sig":
-            filedata_hash, path, signature, filedata_algo, error = \
-                _extract_from_ima_sig(tokens, template_hash)
-        elif mode == 'ima':
-            filedata_hash, path, error = _extract_from_ima(tokens,
-                                                           template_hash)
-        else:
-            raise Exception("unsupported ima template mode: %s" % mode)
+            if not entry.valid():
+                errors[type(entry.mode)] = errors.get(type(entry.mode), 0) + 1
 
-        errs[0] += error
-        if template_hash == START_HASH:
-            template_hash = FF_HASH
+            if not found_pcr:
+                # End of list should equal pcr value
+                found_pcr = (codecs.encode(running_hash, 'hex').decode('utf-8') == pcrval)
 
-        # update hash
-        runninghash = hashlib.sha1(runninghash + template_hash).digest()
-
-        if not found_pcr:
-            found_pcr = \
-                (codecs.encode(runninghash, 'hex').decode('utf-8') == pcrval)
-
-        # write out the new hash
-        if m2w is not None:
-            m2w.write("%s %s\n" %
-                      (codecs.encode(filedata_hash, 'hex').decode('utf-8'),
-                       path))
-
-        evaluated = False
-
-        if signature and ima_keyring:
-            evaluated = True
-            # determine if path matches any exclusion list items
-            if compiled_regex is not None and compiled_regex.match(path):
-                logger.debug("IMA: ignoring excluded path %s" % path)
-                continue
-
-            if not ima_keyring.integrity_digsig_verify(signature, filedata_hash, filedata_algo):
-                logger.warning("signature for file %s is not valid" % (path))
-                errs[3] += 1
-            else:
-                logger.debug("signature for file %s is good" % path)
-
-        if allowlist is not None:
-            # just skip if it is a weird overwritten path
-            if template_hash == FF_HASH:
-                # print "excluding ffhash %s"%path
-                continue
-
-            # determine if path matches any exclusion list items
-            if compiled_regex is not None and compiled_regex.match(path):
-                logger.debug("IMA: ignoring excluded path %s" % path)
-                continue
-
-            accept_list = allowlist.get(path, None)
-            if accept_list is None:
-                # if it's NOT already evaluated by a file signature then a
-                # missing file entry is an error -- this enforces that every
-                # entry is 'covered' by signature or allowlist
-                if not evaluated:
-                    logger.warning("File not found in allowlist: %s" % (path))
-                    errs[1] += 1
-                continue
-
-            # print('codecs.encode', codecs.encode(filedata_hash, 'hex').decode('utf-8'))
-            # print('accept_list:', accept_list)
-            if codecs.encode(filedata_hash, 'hex').decode('utf-8') not in accept_list:
-                logger.warning("Hashes for file %s don't match %s not in %s" %
-                               (path,
-                                codecs.encode(filedata_hash, 'hex').decode('utf-8'),
-                                accept_list))
-                errs[2] += 1
-                continue
-
-            evaluated = True
-
-        if ima_keyring and not evaluated:
-            logger.warning("File %s not evaluated with signature or allowlist" % path)
-            errs[1] += 1
-
-        errs[4] += 1
+            # Keep old functionality for writing the parsed files with hashes into a file
+            if m2w is not None and (type(entry.mode) in [ima_ast.Ima, ima_ast.ImaNg, ima_ast.ImaSig]):
+                hash_value = codecs.encode(entry.mode.digest.hash, "hex")
+                path = entry.mode.path.name
+                m2w.write(f"{hash_value} {path}\n")
+        except ima_ast.ParserError:
+            logger.error(f"Line was not parsable into a valid IMA entry: {line}")
 
     # check PCR value has been found
     if not found_pcr:
         logger.error("IMA measurement list does not match TPM PCR %s" % pcrval)
         return None
 
-    # clobber the retval if there were IMA file errors
-    if sum(errs[:4]) > 0:
-        logger.error(
-            "IMA ERRORS: template-hash %d fnf %d hash %d bad-sig %d good %d" % tuple(errs))
+    # Check if any validators failed
+    if sum(errors.values()) > 0:
+        error_msg = "IMA ERRORS: Some entries couldn't be validated. Number of failures in modes: "
+        error_msg += ", ".join([f'{k.__name__ } {v}' for k, v in errors.items()])
+        logger.error(error_msg + ".")
         return None
 
-    return codecs.encode(runninghash, 'hex').decode('utf-8')
+    return codecs.encode(running_hash, 'hex').decode('utf-8')
 
 
 def process_allowlists(allowlist, exclude):
