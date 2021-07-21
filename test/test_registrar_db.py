5b2a8f6a939cfc42c8a94f4041b2928391a54602
Luke Hinds <lhinds@redhat.com>
2021-07-21 21:30:37 +0100

    Fix wrong test uuid

--- test/test_registrar_db.py
+++ test/test_registrar_db.py
@@ -13,7 +13,7 @@ from keylime.db.keylime_db import SessionManager
 # BEGIN TEST DATA
 
 test_data = {
-    'agent_id': '4d32fbb3-d2f1-4a97-9ef7-75bd81c00000',
+    'agent_id': 'd432fbb3-d2f1-4a97-9ef7-75bd81c00000',
     'ek_tpm': """-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0dLxdAABVJO6qxamjCMh
 yhWZgiFHZHnPEe0tMFyK3fNVr/w8lX9r+QOLxLmkT0IdgsEYtGZGefbD+qQl4O1s
