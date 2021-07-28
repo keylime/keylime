6e86dbb2e7f995f7ecdceabab0022d33fe860e88
Riccardo Pittau <elfosardo@gmail.com>
2021-07-28 15:10:04 +0200

    Use tox for pylint
    Using tox automatically gives a reproduceable environment no matter
    where the test is executed.
    Also move pylint config to pylintrc.
    
    Signed-off-by: Riccardo Pittau <elfosardo@gmail.com>

--- scripts/check_codestyle.sh
+++ scripts/check_codestyle.sh
@@ -13,13 +13,6 @@ fi
 
 ${PYLINT} --version
 
-${PYLINT} \
-  --jobs=0 \
-  --ignored-modules=zmq,alembic.op,alembic.context,M2Crypto.m2,Cryptodome,pylab,matplotlib,numpy \
-  --disable W1509 \
-  --disable C0103,C0115,C0116,C0301,C0302,C0111 \
-  --disable W0102,W0511,W0603,W0703,W1201,W1203 \
-  --disable E1120 \
-  --disable R0801,R0902,R0903,R0904,R0912,R0913,R0914,R0915,R0201,R0911,R1729,R1732 \
-  *.py $(find ./keylime ./test -name '*.py' ! -name 'oldtest.py')
+${PYLINT} *.py $(find ./keylime ./test -name '*.py' ! -name 'oldtest.py')
+
 exit $?
