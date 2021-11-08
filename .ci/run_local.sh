3163e3d99fbdd4e381aa61a42bc586aad6a97bea
Mark Bestavros <mbestavr@redhat.com>
2021-11-08 12:21:16 -0500

    Clean up CI scripts and rewrite run_local.sh
    run_local.sh has been setting up its swtpm differently from how the
    Keylime test suite works on Github Actions for a long while now.
    Notably, the old approach required privileged containers, while the
    newer GHA approach does not. This PR rewrites run_local.sh to mirror the
    GHA setup.
    
    Signed-off-by: Mark Bestavros <mbestavr@redhat.com>

--- .ci/run_local.sh
+++ .ci/run_local.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
-# You can specify the path of the local keylime repository as argument
-# of this script or using the KEYLIME_REPO_PATH environment variable.
-# The default value is /home/${USER}/keylime
-REPO=${KEYLIME_REPO_PATH:-${1:-/home/${USER}/keylime}}
-
 # keylime images
-tpm20image="lukehinds/keylime-ci-tpm20"
-tpm20tag="v101"
+tpmimage="quay.io/keylime/keylime-ci"
+tpmtag="latest"
 
-echo -e "Grabbing latest images"
+echo -e "Fetching Keylime CI container"
 
-docker pull ${tpm20image}:${tpm20tag}
+docker pull ${tpmimage}:${tpmtag}
 
-function tpm2 {
-    container_id=$(mktemp)
-    docker run --detach --privileged \
-        -v $REPO:/root/keylime \
-        -v /sys/fs/cgroup:/sys/fs/cgroup \
-        --cgroupns=host \
-        -it ${tpm20image}:${tpm20tag} >> ${container_id}
-    docker exec -u 0 -it --tty "$(cat ${container_id})" \
-        /bin/bash /root/keylime/.ci/test_wrapper.sh
-    docker stop "$(cat ${container_id})"
-    docker rm "$(cat ${container_id})"
-}
+echo -e "Running Keylime's test suite"
 
-while true; do
-    echo -e ""
-    read -p "Do you wish to test against TPM 2.0(a) or q(quit): " abq
-    case $abq in
-        [a]* ) tpm2;;
-        [q]* ) exit;;
-        * ) echo "Please answer a or q(quit)";;
-    esac
-done
+container_id=$(mktemp)
+docker run --detach --user root --env KEYLIME_TEST='true' \
+    --mount type=tmpfs,destination=/var/lib/keylime/,tmpfs-mode=1770 \
+    -it ${tpmimage}:${tpmtag} >> ${container_id}
+# clone the Keylime repository
+docker exec -u root -it --tty "$(cat ${container_id})" \
+    git clone https://github.com/keylime/keylime.git /root/keylime
+# run the Keylime test suite
+docker exec -u root -it --tty "$(cat ${container_id})" \
+    /bin/bash /root/keylime/.ci/test_wrapper.sh
+docker stop "$(cat ${container_id})"
+docker rm "$(cat ${container_id})"
