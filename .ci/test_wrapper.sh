6f7e6b1ba1dc6e028bf71cee1af4ef8165cf049b
Mark Bestavros <mbestavr@redhat.com>
2021-11-08 12:21:16 -0500

    Clean up CI scripts and rewrite run_local.sh
    run_local.sh has been setting up its swtpm differently from how the
    Keylime test suite works on Github Actions for a long while now.
    Notably, the old approach required privileged containers, while the
    newer GHA approach does not. This PR rewrites run_local.sh to mirror the
    GHA setup.
    
    Signed-off-by: Mark Bestavros <mbestavr@redhat.com>

--- .ci/test_wrapper.sh
+++ .ci/test_wrapper.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 set -x
 
-KEYLIME_HOME="${KEYLIME_HOME=:-/root/keylime}"
-
 # Configure swtpm2
-cd ${KEYLIME_HOME}/scripts
-chmod +x init_tpm_server
-chmod +x tpm_serverd
-install -c tpm_serverd /usr/local/bin/tpm_serverd
-install -c init_tpm_server /usr/local/bin/init_tpm_server
-# Server needs to be running, or tpm2-abrmd.service will fail.
-/usr/local/bin/tpm_serverd
+mkdir /tmp/tpmdir
+swtpm_setup --tpm2 \
+     --tpmstate /tmp/tpmdir \
+     --createek --decryption --create-ek-cert \
+     --create-platform-cert \
+     --display
+swtpm socket --tpm2 \
+     --tpmstate dir=/tmp/tpmdir \
+     --flags startup-clear \
+     --ctrl type=tcp,port=2322 \
+     --server type=tcp,port=2321 \
+     --daemon
+export TPM2TOOLS_TCTI=tabrmd:
 
-export TPM2TOOLS_TCTI="tabrmd:bus_name=com.intel.tss2.Tabrmd"
-pkill -HUP dbus-daemon
+# Configure dbus
+sudo rm -rf /var/run/dbus
+sudo mkdir /var/run/dbus
+sudo dbus-daemon --system
 
-# Configure tpm2-abrmd systemd
-# systemd-udev-settle is not needed to run tpm2-tools in container
-sed -i 's/^After/#&/' /usr/lib/systemd/system/tpm2-abrmd.service
-sed -i 's/^Requires/#&/' /usr/lib/systemd/system/tpm2-abrmd.service
-# the tpm device is not needed to run tpm2-tools in container with the
-# tpm emulator
-sed -i 's/^ConditionPathExists/#&/' /usr/lib/systemd/system/tpm2-abrmd.service
-sed -i 's/.*ExecStart.*/ExecStart=\/usr\/sbin\/tpm2-abrmd --tcti=mssim/' /usr/lib/systemd/system/tpm2-abrmd.service
-systemctl daemon-reload
-systemctl enable tpm2-abrmd
-systemctl start tpm2-abrmd
-# Check that tpm2-abrmd is actually running as starting it won't report failures
-if ! (systemctl -q is-active tpm2-abrmd); then
-    echo "tpm2-abrmd failed to start, exiting"
-    exit 1
-fi
+tpm2-abrmd \
+    --logger=stdout \
+    --tcti=swtpm: \
+    --flush-all \
+    --allow-root &
 
 # Run tests
-cd ${KEYLIME_HOME}/test
-chmod +x ./run_tests.sh
-./run_tests.sh -s openssl
+if [ "$GITHUB_ACTIONS" == "true" ]
+then
+    REPO_DIR=$GITHUB_WORKSPACE
+else
+    REPO_DIR="/root/keylime"
+fi
+chmod +x $REPO_DIR/test/run_tests.sh
+$REPO_DIR/test/run_tests.sh -s openssl
