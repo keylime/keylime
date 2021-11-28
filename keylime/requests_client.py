f58355bf2272033dde4d6bc5f5e986dfa0684011
Thore Sommer <mail@thson.de>
2021-11-28 09:48:25 +0100

    request_client: add the option to ignore hostname validation
    For most certificates we do not care about the hostname
    
    Signed-off-by: Thore Sommer <mail@thson.de>

--- keylime/requests_client.py
+++ keylime/requests_client.py
@@ -5,14 +5,20 @@ Copyright 2017 Massachusetts Institute of Technology.
 
 import requests
 
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager  # pylint: disable=import-error
+
 
 class RequestsClient:
-    def __init__(self, base_url, tls_enabled, **kwargs):
+    def __init__(self, base_url, tls_enabled, ignore_hostname=False, **kwargs):
         if tls_enabled:
             self.base_url = f'https://{base_url}'
         else:
             self.base_url = f'http://{base_url}'
         self.session = requests.Session()
+        if ignore_hostname:
+            self.session.mount("http://", HostNameIgnoreAdapter())
+            self.session.mount("https://", HostNameIgnoreAdapter())
         for arg, value in kwargs.items():
             if isinstance(value, dict):
                 value = self.__deep_merge(
@@ -49,3 +55,17 @@ class RequestsClient:
             else:
                 destination[key] = value
         return destination
+
+
+class HostNameIgnoreAdapter(HTTPAdapter):
+    """
+    This HTTPAdapter just ignores the Hostname validation.
+
+    It is required because in most cases we don't know the hostname during certificate generation.
+    """
+    def init_poolmanager(self, connections, maxsize, block=requests.adapters.DEFAULT_POOLBLOCK, **pool_kwargs):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       strict=True,
+                                       assert_hostname=False, **pool_kwargs)
