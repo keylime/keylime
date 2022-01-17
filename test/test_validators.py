983d702df36e5b6554edbfc267f9e2d7a0d2d34a
Alberto Planas <aplanas@suse.com>
2022-01-17 15:04:26 +0100

    validators: add uuid and agent_id validators
    Add valid_uuid and valid_agent_id validators, and tests for both.
    
    Signed-off-by: Alberto Planas <aplanas@suse.com>

--- test/test_validators.py
+++ test/test_validators.py
@@ -78,5 +78,49 @@ class TestValidHex(unittest.TestCase):
         self.assertFalse(validators.valid_hex("123xyz"))
 
 
+class TestValidUUID(unittest.TestCase):
+    """Tests for valid_uuid."""
+
+    def test_none(self):
+        """Check that None is not valid."""
+        self.assertFalse(validators.valid_uuid(None))
+
+    def test_empty(self):
+        """Check that the empty string is not valid."""
+        self.assertFalse(validators.valid_uuid(""))
+
+    def test_valid(self):
+        """Check a valid UUID that mix upper and lower case."""
+        self.assertTrue(validators.valid_uuid("74a93e15-da24-4ff1-ABC0-55beed02a16a"))
+
+    def test_invalid(self):
+        """Check an invalid UUID that mix upper and lower case."""
+        self.assertFalse(validators.valid_uuid("some text"))
+
+
+class TestValidAgentID(unittest.TestCase):
+    """Tests for valid_agent_id."""
+
+    def test_none(self):
+        """Check that None is not valid."""
+        self.assertFalse(validators.valid_agent_id(None))
+
+    def test_empty(self):
+        """Check that the empty string is not valid."""
+        self.assertFalse(validators.valid_agent_id(""))
+
+    def test_valid_uuid(self):
+        """Check a valid UUID that mix upper and lower case."""
+        self.assertTrue(validators.valid_agent_id("74a93e15-da24-4ff1-ABC0-55beed02a16a"))
+
+    def test_valid_hostname(self):
+        """Check a valid hostname that mix upper and lower case."""
+        self.assertTrue(validators.valid_agent_id("my-Hostname.example.com"))
+
+    def test_invalid(self):
+        """Check an invalid user ID with non valid characters."""
+        self.assertFalse(validators.valid_agent_id("rm -fr *"))
+
+
 if __name__ == "__main__":
     unittest.main()
