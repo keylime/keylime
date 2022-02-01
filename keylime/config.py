3bc8791bcbf751ca601174ac0c8d21db7ce4d1cc
Thore Sommer <mail@thson.de>
2022-02-01 16:06:41 +0100

    setup.py, config: install default configuration into package path
    The keylime.conf is copied into the package path instead of trying to
    install it globally into /etc which fails if the user is non root.
    
    We will fallback on this configuration the other potential ones do not
    exist.
    
    We now also pass the parsed config directly to the logging config.
    
    Signed-off-by: Thore Sommer <mail@thson.de>

--- keylime/config.py
+++ keylime/config.py
@@ -7,6 +7,13 @@ import os.path
 import configparser
 from typing import Optional
 
+# resources in importlib is a Python 3.7 feature, so we disable fallback support if we cannot import this module
+try:
+    from importlib import resources
+    _CONFIG_FALLBACK = True
+except ImportError:
+    _CONFIG_FALLBACK = False
+
 import yaml
 try:
     from yaml import CSafeLoader as SafeLoader
@@ -120,18 +127,24 @@ if "KEYLIME_CONFIG" in os.environ:
 def get_config():
     """Read configuration files and merge them together."""
     if not getattr(get_config, "config", None):
+        # Use RawConfigParser, so we can also use it as the logging config
+        config = configparser.RawConfigParser()
         # TODO - use logger and be sure that all variables have a
         # propper default, and the sections are initialized
         if not any(os.path.exists(c) for c in CONFIG_FILES):
             print(f"Config file not found in {CONFIG_FILES}. Please set "
                   f"environment variable KEYLIME_CONFIG or see {__file__} "
-                  "for more details")
-
-        # Validate that at least one config file is present
-        get_config.config = configparser.ConfigParser()
-        config_files = get_config.config.read(CONFIG_FILES)
-        # TODO - use the logger
-        print(f"Reading configuration from {config_files}")
+                  "for more details.")
+            if _CONFIG_FALLBACK:
+                print("Falling back on package provided configuration")
+                file = resources.files(__package__).joinpath("keylime.conf")
+                config.read_string(file.read_text("utf-8"))
+        else:
+            # Validate that at least one config file is present
+            config_files = config.read(CONFIG_FILES)
+            # TODO - use the logger
+            print(f"Reading configuration from {config_files}")
+        get_config.config = config
     return get_config.config
 
 
