99f444dc46861e9dc9eadaadd5267f5e14164f3c
Thore Sommer <mail@thson.de>
2022-03-15 21:42:36 +0100

    services: make the services run as keylime user instead of root
    The services for the registrar and verifier are changed to run as keylime
    user. The agent starts as root but the installer.sh changes the "run_as"
    option to drop privileges to keylime:tss.
    
    The installer.sh is changed to create the keylime user, create the correct
    directories, change the permissions to the keylime user and to also install
    the secure mount point.
    
    Signed-off-by: Thore Sommer <mail@thson.de>

--- services/installer.sh
+++ services/installer.sh
@@ -24,12 +24,40 @@ sed "s|KEYLIMEDIR|$KEYLIMEDIR|g" $BASEDIR/keylime_agent.service.template > /etc/
 sed "s|KEYLIMEDIR|$KEYLIMEDIR|g" $BASEDIR/keylime_registrar.service.template > /etc/systemd/system/keylime_registrar.service
 sed "s|KEYLIMEDIR|$KEYLIMEDIR|g" $BASEDIR/keylime_verifier.service.template > /etc/systemd/system/keylime_verifier.service
 
+# Copy secure mount
+cp keylime_agent_secure.mount  /etc/systemd/system/keylime_agent_secure.mount
+
+echo "Creating keylime user if it not exists"
+if ! getent passwd keylime >/dev/null; then
+    adduser --system --shell /bin/false \
+            --home /var/lib/keylime --no-create-home \
+            keylime
+fi
+
+echo "Change /etc/keylime.conf to enable privilege dropping for the agent"
+sed -i "s/run_as =/run_as = keylime:tss/g" /etc/keylime.conf
+
+echo "Changing files to be owned by the keylime user"
+# Create all directories required if not there
+mkdir -p /var/lib/keylime
+mkdir -p /var/log/keylime
+mkdir -p /var/run/keylime
+
+chown keylime:keylime /etc/keylime.conf
+chown keylime:keylime -R /var/lib/keylime
+chown keylime:keylime -R /var/log/keylime
+chown keylime:keylime -R /var/run/keylime
+
 # set permissions
 chmod 664 /etc/systemd/system/keylime_agent.service
 chmod 664 /etc/systemd/system/keylime_registrar.service
 chmod 664 /etc/systemd/system/keylime_verifier.service
+chmod 666 /etc/systemd/system/keylime_agent_secure.mount
+
+chmod 700 /var/run/keylime
 
 # enable at startup
 systemctl enable keylime_agent.service
 systemctl enable keylime_registrar.service
 systemctl enable keylime_verifier.service
+systemctl enable keylime_agent_secure.mount
