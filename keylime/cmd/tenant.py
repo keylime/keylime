85b0cf117b201a818bfc276df13c7d7edead8b0a
Michael Peters <mpeters@redhat.com>
2022-03-22 10:35:13 -0400

    keylime_tenant non-zero exit code on error
    Now that we are catching errors and making them more user-friendly
    we still need to exit with a non-zero exit code so that other
    programs can tell when something went wrong
    
    Signed-off-by: Michael Peters <mpeters@redhat.com>

--- keylime/cmd/tenant.py
+++ keylime/cmd/tenant.py
@@ -5,6 +5,8 @@ SPDX-License-Identifier: Apache-2.0
 Copyright 2017 Massachusetts Institute of Technology.
 '''
 
+import sys
+
 from keylime import keylime_logging
 from keylime import tenant
 
@@ -16,8 +18,10 @@ def main():
         tenant.main()
     except tenant.UserError as ue:
         logger.error(str(ue))
+        sys.exit(1)
     except Exception as e:
         logger.exception(e)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
