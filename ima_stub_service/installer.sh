805a00649526af2f7fce5fb25d7f0ecf185b51ce
Thore Sommer <mail@thson.de>
2022-03-31 12:27:06 +0200

    tpm_emulator: move everything into systemd services
    The IMA emulator is now also a systemd service, which depends on the
    TPM emulator.
    
    Signed-off-by: Thore Sommer <mail@thson.de>

--- ima_stub_service/installer.sh
+++ ima_stub_service/installer.sh
@@ -9,16 +9,16 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-cp tpm_with_ima.sh /usr/local/bin/
-chmod 744 /usr/local/bin/tpm_with_ima.sh
-
 if [[ -n `systemctl 2>&1 > /dev/null` ]]; then
-	echo "No systemd on this system, starting TPM emulator service manually"
-	/usr/local/bin/tpm_with_ima.sh > /dev/null
-else
-	# create service
-	cp tpm_emulator.service /etc/systemd/system
-	chmod 664 /etc/systemd/system/tpm_emulator.service
-	systemctl enable tpm_emulator.service
-	systemctl start tpm_emulator.service
+	echo "No systemd on this system! Will not install emulator services"
+	exit 1
 fi
+
+# create services and socket
+for service in "tpm_emulator.service" "ima_emulator.service"; do
+  cp  ${service} /etc/systemd/system
+  chmod 644 /etc/systemd/system/${service}
+  systemctl enable ${service}
+done
+
+systemctl start ima_emulator.service
