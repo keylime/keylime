a71cfcc71354abf5cada97e4c129e1f419d4fe4d
Karel Srot <ksrot@redhat.com>
2022-06-08 21:17:51 +0200

    IGNORE: Testing PR for codecov.io coverage measurement
    Signed-off-by: Karel Srot <ksrot@redhat.com>

--- keylime/signing.py
+++ keylime/signing.py
@@ -3,9 +3,7 @@ SPDX-License-Identifier: Apache-2.0
 Copyright 2017 Massachusetts Institute of Technology.
 """
 
-import tempfile
-
-import gnupg
+import gpg
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
@@ -30,7 +28,13 @@ def verify_signature_from_file(key_file, filename, sig_file, file_description):
     with open(filename, "rb") as file_f:
         file = file_f.read()
 
-    if verify_signature(key, sig, file):
+    verified = False
+    try:
+        verified = verify_signature(key, sig, file)
+    except Exception as e:
+        logger.debug("Unable to verify signature: %s", e)
+
+    if verified:
         logger.debug("%s passed signature verification", file_description.capitalize())
     else:
         raise Exception(
@@ -49,19 +53,17 @@ def verify_signature(key, sig, file):
 
     # PGP
     if key_header == "-----BEGIN PGP PUBLIC KEY BLOCK-----":
-        gpg = gnupg.GPG()
-        logger.debug("Importing GPG key")
-        gpg_imported = gpg.import_keys(key.decode("utf-8"))
-        if gpg_imported.count == 1:  # pylint: disable=E1101
-            logger.debug("GPG key successfully imported")
-        else:
-            raise Exception("Unable to import GPG key")
+        verified = False
+        ctx = gpg.Context()
+        try:
+            logger.debug("Importing GPG key")
+            result = ctx.key_import(key)
+        except Exception as e:
+            raise Exception("Unable to import GPG key") from e
 
-        # The Python PGP library won't let you read a signature from memory, hence this hack.
-        with tempfile.NamedTemporaryFile() as temp_sig:
-            temp_sig.write(sig)
-            temp_sig.flush()
-            verified = gpg.verify_data(temp_sig.name, file)
+        if result is not None and hasattr(result, "considered") is True:
+            _, result = ctx.verify(file, sig)
+            verified = result.signatures[0].status == 0
 
     # OpenSSL
     elif key_header == "-----BEGIN PUBLIC KEY-----":
