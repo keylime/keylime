f8991852b8f50caa8fe1f95d24c9e4835f5dd373
Avery Blanchard <ablancha@redhat.com>
2022-07-26 12:37:01 -0400

    Keep original control structure, minimize change
    Signed-off-by: Avery Blanchard <ablancha@redhat.com>

--- installer.sh
+++ installer.sh
@@ -76,44 +76,8 @@ case "$ID" in
         esac
     ;;
 
-    centos)
+    rhel | centos)
         case "${VERSION_ID}" in
-            7*)
-                echo "${ID} ${VERSION_ID} selected."
-                PACKAGE_MGR=$(command -v yum)
-                NEED_EPEL=1
-                PYTHON_PREIN="python36 python36-devel python36-setuptools python36-pip git wget patch openssl"
-                PYTHON_DEPS="gcc gcc-c++ openssl-devel swig python36-PyYAML python36-tornado python36-cryptography python36-requests python36-zmq yaml-cpp-devel python3-psutil python3-alembic"
-                if [ "$(uname -m)" = "x86_64" ]; then
-                    PYTHON_DEPS+=" efivar-libs"
-                fi
-                BUILD_TOOLS="openssl-devel file libtool make automake m4 libgcrypt-devel autoconf autoconf-archive libcurl-devel libstdc++-devel uriparser-devel dbus-devel gnulib-devel doxygen libuuid-devel json-c-devel"
-                NEED_BUILD_TOOLS=1
-                CENTOS7_TSS_FLAGS="--enable-esapi=no --disable-doxygen-doc"
-            ;;
-            8*)
-                echo "${ID} ${VERSION_ID} selected."
-                PACKAGE_MGR=$(command -v dnf)
-                NEED_EPEL=1
-                PYTHON_PREIN="python3 python3-devel python3-setuptools python3-pip"
-                PYTHON_DEPS="gcc gcc-c++ openssl-devel python3-yaml python3-requests swig python3-cryptography wget git python3-tornado python3-zmq python3-gpg python3-psutil"
-                if [ "$(uname -m)" = "x86_64" ]; then
-                    PYTHON_DEPS+=" efivar-libs"
-                fi
-                BUILD_TOOLS="git wget patch libyaml openssl-devel libtool make automake m4 libgcrypt-devel autoconf libcurl-devel libstdc++-devel dbus-devel libuuid-devel json-c-devel"
-                #TPM2_TOOLS_PKGS="tpm2-tss tpm2-tools" TODO: still on 3.1.1 tpm2_tools
-                NEED_BUILD_TOOLS=1
-                NEED_PYTHON_DIR=1
-		POWERTOOLS="--enablerepo=PowerTools install autoconf-archive"
-            ;;
-	     *)
-                echo "Version ${VERSION_ID} of ${ID} not supported"
-                exit 1
-        esac
-    ;;
-
-    rhel) 
-	case "${VERSION_ID}" in
             7*)
                 echo "${ID} ${VERSION_ID} selected."
                 PACKAGE_MGR=$(command -v yum)
@@ -140,9 +104,14 @@ case "$ID" in
                 #TPM2_TOOLS_PKGS="tpm2-tss tpm2-tools" TODO: still on 3.1.1 tpm2_tools
                 NEED_BUILD_TOOLS=1
                 NEED_PYTHON_DIR=1
-		POWERTOOLS="config-manager --set-enabled rhui-codeready-builder-for-rhel-8-x86_64-rhui-source-rpms"
+		if test "$ID" = centos; then
+			POWERTOOLS="--enablerepo=PowerTools"
+		else
+			POWERTOOLS="config-manager --set-enabled rhui-codeready-builder-for-rhel-8-x86_64-rhui-source-rpms"
+		fi 
+
             ;;
-            *)
+	     *)
                 echo "Version ${VERSION_ID} of ${ID} not supported"
                 exit 1
         esac
