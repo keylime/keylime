d2ddf4e0ce2cc8e1224f874090f9efab8a02b63b
Michael Peters <mpeters@redhat.com>
2022-09-21 10:50:03 -0400

    Bump up versions to 6.5.0
    Signed-off-by: Michael Peters <mpeters@redhat.com>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Keylime Developers"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "6.4.3"
+release = "6.5.0"
 
 
 # -- General configuration ---------------------------------------------------
