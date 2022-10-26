aa33b3a44dc0add64fd5a5f6900462ac05d1b1aa
Sergio Correia <scorreia@redhat.com>
2022-10-26 10:28:59 -0300

    Make the tpm cert store path configurable
    In both check_tpm_cert_store and cert_loader().
    This will make it simpler for testing the certs in the store.
    
    Additionaly, let's use a dict containing the file name as the key
    for the cert data, as it may be helpful when debugging cert-related
    issues.
    
    Signed-off-by: Sergio Correia <scorreia@redhat.com>

--- keylime/tpm_ek_ca.py
+++ keylime/tpm_ek_ca.py
@@ -7,8 +7,7 @@ logger = keylime_logging.init_logging("tpm_ek_ca")
 trusted_certs = {}
 
 
-def check_tpm_cert_store():
-    tpm_cert_store = config.get("tenant", "tpm_cert_store")
+def check_tpm_cert_store(tpm_cert_store=config.get("tenant", "tpm_cert_store")):
     if not os.path.isdir(tpm_cert_store):
         logger.error("The directory %s does not exist.", tpm_cert_store)
         raise Exception(f"The directory {tpm_cert_store} does not exist.")
@@ -21,11 +20,10 @@ def check_tpm_cert_store():
         raise Exception(f"The directory {tpm_cert_store} does not contain " f"any .pem files")
 
 
-def cert_loader():
-    tpm_cert_store = config.get("tenant", "tpm_cert_store")
+def cert_loader(tpm_cert_store=config.get("tenant", "tpm_cert_store")):
     file_list = glob.glob(os.path.join(tpm_cert_store, "*.pem"))
-    my_trusted_certs = []
+    my_trusted_certs = {}
     for file_path in file_list:
         with open(file_path, encoding="utf-8") as f_input:
-            my_trusted_certs.append(f_input.read())
+            my_trusted_certs[file_path] = f_input.read()
     return my_trusted_certs
