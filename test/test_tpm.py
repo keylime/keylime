e1ca5085f11dc9b387e9df7941567c719bb47cf8
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2022-11-09 13:49:05 +0100

    Test binary measured boot event log parsing
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- test/test_tpm.py
+++ test/test_tpm.py
@@ -1,5 +1,7 @@
+import os
 import unittest
 
+from keylime.common.algorithms import Hash
 from keylime.tpm.tpm_main import tpm
 
 # ############################################################
@@ -90,6 +92,22 @@ class TestTPM(unittest.TestCase):
         for test in tpm_manufacturer_tests:
             response = self.tpm.get_tpm_manufacturer(output=test["challenge"])
             self.assertEqual(test["response"], response)
+
+    # test parsing binary measured boot event log
+    def test_parse_mb_bootlog(self):
+        # Use the file that triggered https://github.com/keylime/keylime/issues/1153
+        mb_log_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "data/mb_log.b64"))
+        with open(mb_log_path, encoding="utf-8") as f:
+            # Read the base64 input and remove the newlines
+            b64 = "".join(f.read().splitlines())
+            pcr_hashes, boot_aggregates, measurement_data, failure = self.tpm.parse_mb_bootlog(b64, Hash.SHA256)
+
+            self.assertFalse(
+                failure, f"Parsing of measured boot log failed with: {list(map(lambda x: x.context, failure.events))}"
+            )
+            self.assertTrue(isinstance(pcr_hashes, dict))
+            self.assertTrue(isinstance(boot_aggregates, dict))
+            self.assertTrue(isinstance(measurement_data, dict))
 
 
 if __name__ == "__main__":
