f056c86b323e6ee4e0b4a3e1f73dca5b835d5690
Thore Sommer <mail@thson.de>
2022-11-29 14:16:43 +0200

    json: allow ignore comment to be parsed by mypy
    Signed-off-by: Thore Sommer <mail@thson.de>

--- keylime/json.py
+++ keylime/json.py
@@ -5,7 +5,7 @@ JSONType = Union[str, int, float, bool, None, Dict[str, Any], List[Any]]
 
 _list_types = [list, tuple]
 try:
-    from sqlalchemy.engine.row import Row  # type: ignore  ;sqlalchemy >= 1.4.
+    from sqlalchemy.engine.row import Row  # type: ignore  # sqlalchemy >= 1.4.
 
     _list_types.append(Row)
 except ModuleNotFoundError:
