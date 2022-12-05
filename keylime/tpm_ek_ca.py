01a7da0e6c852f5bd6a1d232139b4746b643f4e5
Stefan Berger <stefanb@linux.ibm.com>
2022-12-05 10:51:50 -0500

    codestyle: Type-annotate tpm_ek_ca.py and add to mypy
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- keylime/tpm_ek_ca.py
+++ keylime/tpm_ek_ca.py
@@ -1,13 +1,14 @@
 import glob
 import os
+from typing import Dict
 
 from keylime import keylime_logging
 
 logger = keylime_logging.init_logging("tpm_ek_ca")
-trusted_certs = {}
+trusted_certs: Dict[str, str] = {}
 
 
-def check_tpm_cert_store(tpm_cert_store):
+def check_tpm_cert_store(tpm_cert_store: str) -> None:
     if not os.path.isdir(tpm_cert_store):
         logger.error("The directory %s does not exist.", tpm_cert_store)
         raise Exception(f"The directory {tpm_cert_store} does not exist.")
@@ -20,7 +21,7 @@ def check_tpm_cert_store(tpm_cert_store):
         raise Exception(f"The directory {tpm_cert_store} does not contain " f"any .pem files")
 
 
-def cert_loader(tpm_cert_store):
+def cert_loader(tpm_cert_store: str) -> Dict[str, str]:
     file_list = glob.glob(os.path.join(tpm_cert_store, "*.pem"))
     my_trusted_certs = {}
     for file_path in file_list:
