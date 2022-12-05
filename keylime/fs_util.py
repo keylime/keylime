b6c66a257ee3e43d1d749e9e9919a8eb4c529af1
Stefan Berger <stefanb@linux.ibm.com>
2022-12-05 10:37:39 -0500

    codestyle: Type-annotate fs_util.py and add to mypy
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- keylime/fs_util.py
+++ keylime/fs_util.py
@@ -3,7 +3,7 @@
 import os
 
 
-def ch_dir(path):
+def ch_dir(path: str) -> None:
     """Change directory and create it if missing."""
     if not os.path.exists(path):
         os.makedirs(path, 0o700)
