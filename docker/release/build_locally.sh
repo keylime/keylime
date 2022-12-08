532cdfb4bbe890f7fed898aa322920b175134d55
Thore Sommer <mail@thson.de>
2022-12-08 11:42:29 +0200

    docker: fix and improve build_locally.sh
    Cotainers are now tagged with the correct version. Plain logging is now used
    and there is the option to pass parameters to the buildx command.
    
    Signed-off-by: Thore Sommer <mail@thson.de>

--- docker/release/build_locally.sh
+++ docker/release/build_locally.sh
@@ -9,6 +9,6 @@ KEYLIME_DIR=${2:-"../../"}
 
 ./generate-files.sh ${VERSION}
 for part in base registrar verifier tenant; do
-  docker buildx build -t keylime_${part} -f ${part}/Dockerfile $KEYLIME_DIR
+  docker buildx build -t keylime_${part}:${VERSION} -f ${part}/Dockerfile $KEYLIME_DIR --progress plain ${@:3}
   rm -f ${part}/Dockerfile
 done
