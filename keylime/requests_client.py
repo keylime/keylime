04562b98c7e82aacec7deb02bae3aa58f4ae79c0
Stefan Berger <stefanb@linux.ibm.com>
2022-12-24 10:02:47 -0500

    codestyle: Add type annotations to requests_client.py and add to mypy
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- keylime/requests_client.py
+++ keylime/requests_client.py
@@ -1,10 +1,20 @@
+import ssl
+from typing import Any, Dict, Optional
+
 import requests
 from requests.adapters import DEFAULT_POOLBLOCK, HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager  # pylint: disable=import-error
 
 
 class RequestsClient:
-    def __init__(self, base_url, tls_enabled, tls_context=None, ignore_hostname=True, **kwargs):
+    def __init__(
+        self,
+        base_url: str,
+        tls_enabled: bool,
+        tls_context: Optional[ssl.SSLContext] = None,
+        ignore_hostname: bool = True,
+        **kwargs: Any,
+    ) -> None:
         if tls_enabled:
             self.base_url = f"https://{base_url}"
         else:
@@ -21,35 +31,35 @@ class RequestsClient:
                 value = self.__deep_merge(getattr(self.session, arg), value)
             setattr(self.session, arg, value)
 
-    def __enter__(self):
+    def __enter__(self) -> "RequestsClient":
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
         pass
 
-    def request(self, method, url, **kwargs):
+    def request(self, method: str, url: str, **kwargs: Any) -> requests.Response:
         return self.session.request(method, self.base_url + url, **kwargs)
 
-    def head(self, url, **kwargs):
+    def head(self, url: str, **kwargs: Any) -> requests.Response:
         return self.session.head(self.base_url + url, **kwargs)
 
-    def get(self, url, **kwargs):
+    def get(self, url: str, **kwargs: Any) -> requests.Response:
         return self.session.get(self.base_url + url, **kwargs)
 
-    def post(self, url, **kwargs):
+    def post(self, url: str, **kwargs: Any) -> requests.Response:
         return self.session.post(self.base_url + url, **kwargs)
 
-    def put(self, url, **kwargs):
+    def put(self, url: str, **kwargs: Any) -> requests.Response:
         return self.session.put(self.base_url + url, **kwargs)
 
-    def patch(self, url, **kwargs):
+    def patch(self, url: str, **kwargs: Any) -> requests.Response:
         return self.session.patch(self.base_url + url, **kwargs)
 
-    def delete(self, url, **kwargs):
+    def delete(self, url: str, **kwargs: Any) -> requests.Response:
         return self.session.delete(self.base_url + url, **kwargs)
 
     @staticmethod
-    def __deep_merge(source, destination):
+    def __deep_merge(source: Dict[Any, Any], destination: Dict[Any, Any]) -> Dict[Any, Any]:
         for key, value in source.items():
             if isinstance(value, dict):
                 node = destination.setdefault(key, {})
@@ -66,13 +76,15 @@ class HostNameIgnoreAdapter(HTTPAdapter):
     It is required because in most cases we don't know the hostname during certificate generation.
     """
 
-    def __init__(self, tls_context, ignore_hostname, *args, **kwargs):
+    def __init__(self, tls_context: Optional[ssl.SSLContext], ignore_hostname: bool, *args: Any, **kwargs: Any) -> None:
         self._tls_context = tls_context
         self._ignore_hostname = ignore_hostname
 
         super().__init__(*args, **kwargs)
 
-    def init_poolmanager(self, connections, maxsize, block=DEFAULT_POOLBLOCK, **pool_kwargs):
+    def init_poolmanager(
+        self, connections: int, maxsize: int, block: bool = DEFAULT_POOLBLOCK, **pool_kwargs: Any
+    ) -> None:
         assert_hostname = False if self._ignore_hostname and self._tls_context else None
         self.poolmanager = PoolManager(
             num_pools=connections,
