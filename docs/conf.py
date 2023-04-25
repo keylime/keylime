d45ccdac6e6a9deacf3129fe7554aa83f37c6a5d
Marcio Silva <marcio.a.silva@ibm.com>
2023-04-25 12:13:25 -0400

    bump to version 6.8.0
    Signed-off-by: Marcio Silva <marcio.a.silva@ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Keylime Developers"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "6.6.0"
+release = "6.8.0"
 
 
 # -- General configuration ---------------------------------------------------
