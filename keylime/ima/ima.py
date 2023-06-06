378882b4e7f80d959b40842254e81850755a41a6
Thore Sommer <mail@thson.de>
2023-06-06 20:03:15 +0300

    ima: remove code used for reading the IMA log from disk
    Signed-off-by: Thore Sommer <mail@thson.de>

--- keylime/ima/ima.py
+++ keylime/ima/ima.py
@@ -3,7 +3,7 @@ import enum
 import functools
 import hashlib
 import json
-from typing import Any, Dict, List, Optional, Pattern, Set, TextIO, Tuple, Type
+from typing import Any, Dict, List, Optional, Pattern, Tuple, Type
 
 import jsonschema
 
@@ -102,94 +102,6 @@ RUNTIME_POLICY_SCHEMA = {
         }
     },
 }
-
-
-class IMAMeasurementList:
-    """IMAMeasurementList models the IMA measurement lists's last known
-    two numbers of entries and filesizes
-    """
-
-    instance = None
-    entries: Set[Tuple[int, int]]
-
-    @staticmethod
-    def get_instance() -> "IMAMeasurementList":
-        """Return a singleton"""
-        if not IMAMeasurementList.instance:
-            IMAMeasurementList.instance = IMAMeasurementList()
-        return IMAMeasurementList.instance
-
-    def __init__(self) -> None:
-        """Constructor"""
-        self.entries = set()
-        self.reset()
-
-    def reset(self) -> None:
-        """Reset the variables"""
-        self.entries = set()
-
-    def update(self, num_entries: int, filesize: int) -> None:
-        """Update the number of entries and current filesize of the log."""
-        if len(self.entries) > 256:
-            for entry in self.entries:
-                self.entries.discard(entry)
-                break
-        self.entries.add((num_entries, filesize))
-
-    def find(self, nth_entry: int) -> Tuple[int, int]:
-        """Find the closest entry to the n-th entry and return its number
-        and filesize to seek to, return 0, 0 if nothing was found.
-        """
-        best = (0, 0)
-        for entry in self.entries:
-            if entry[0] > best[0] and entry[0] <= nth_entry:
-                best = entry
-        return best
-
-
-def read_measurement_list(ima_log_file: TextIO, nth_entry: int) -> Tuple[Optional[str], int, int]:
-    """Read the IMA measurement list starting from a given entry.
-    The entry may be of any value 0 <= entry <= entries_in_log where
-    entries_in_log + 1 indicates that the client wants to read the next entry
-    once available. If the entry is outside this range, the function will
-    automatically read from the 0-th entry.
-    This function returns the measurement list and the entry from where it
-    was read and the current number of entries in the file.
-    """
-    IMAML = IMAMeasurementList.get_instance()
-    ml = None
-
-    # Try to find the closest entry to the nth_entry
-    num_entries, filesize = IMAML.find(nth_entry)
-
-    if not ima_log_file:
-        IMAML.reset()
-        nth_entry = 0
-        logger.warning("IMA measurement list not available: %s", config.IMA_ML)
-    else:
-        ima_log_file.seek(filesize)
-        filedata = ima_log_file.read()
-        # filedata now corresponds to starting list at entry number 'IMAML.num_entries'
-        # find n-th entry and determine number of total entries in file now
-        offset = 0
-        while True:
-            try:
-                if nth_entry == num_entries:
-                    ml = filedata[offset:]
-                o = filedata.index("\n", offset)
-                offset = o + 1
-                num_entries += 1
-            except ValueError:
-                break
-        # IMAML.filesize corresponds to position for entry number 'IMAML.num_entries'
-        IMAML.update(num_entries, filesize + offset)
-
-        # Nothing found? User request beyond next-expected entry.
-        # Start over with entry 0. This cannot recurse again.
-        if ml is None:
-            return read_measurement_list(ima_log_file, 0)
-
-    return ml, nth_entry, num_entries
 
 
 def _validate_ima_ng(
