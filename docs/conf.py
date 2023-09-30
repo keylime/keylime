1370326be6ec28ea785513f3c929f49da59c5fbd
Marcio Silva <marcio.a.silva@ibm.com>
2023-09-30 09:29:32 -0300

    Monthly release (7.6.0)
    Signed-off-by: Marcio Silva <marcio.a.silva@ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Keylime Developers"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "7.5.0"
+release = "7.6.0"
 
 
 # -- General configuration ---------------------------------------------------
