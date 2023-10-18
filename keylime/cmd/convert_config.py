ce99d3fd7a1aba3fe2f2ea266b5bfc1f83e0ccb1
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2023-10-18 15:27:16 +0200

    convert_config: Do not require keylime modules
    Duplicate the str_to_version() function to avoid requiring keylime
    modules. This makes it possible to run the configuration upgrade script
    without having the keylime modules installed.
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- keylime/cmd/convert_config.py
+++ keylime/cmd/convert_config.py
@@ -85,15 +85,14 @@ import itertools
 import json
 import logging
 import os
+import re
 import shutil
 from configparser import RawConfigParser
 from logging import Logger
 from pathlib import Path
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Union
 
 from jinja2 import Template
-
-from keylime.common.version import str_to_version
 
 COMPONENTS = ["agent", "verifier", "tenant", "registrar", "ca", "logging"]
 
@@ -125,6 +124,26 @@ if "KEYLIME_CA_CONFIG" in os.environ:
 
 if "KEYLIME_LOGGING_CONFIG" in os.environ:
     CONFIG_FILES.insert(0, os.environ["KEYLIME_LOGGING_CONFIG"])
+
+
+def str_to_version(v_str: str) -> Union[Tuple[int, int], None]:
+    """
+    Validates the string format and converts the provided string to a tuple of
+    ints which can be sorted and compared.
+
+    :returns: Tuple with version number parts converted to int. In case of
+    invalid version string, returns None
+    """
+
+    # Strip to remove eventual quotes and spaces
+    v_str = v_str.strip('" ')
+
+    m = re.match(r"^(\d+)\.(\d+)$", v_str)
+
+    if not m:
+        return None
+
+    return (int(m.group(1)), int(m.group(2)))
 
 
 def get_config(config_files: List[List[str]], logger: Logger = logging.getLogger(__name__)) -> RawConfigParser:
