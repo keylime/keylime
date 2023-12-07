keylime/keylime

commit 8b5decd075cbc04868321f884144dd80ca957372

Fix pylint C0103 (naming) errors in some files
Signed-off-by: MC Dubbs <mcdubbs@gmail.com>

docs/conf.py
@@ -19,14 +19,14 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Keylime Documentation"
-copyright = "2022, Keylime Developers"
-author = "Keylime Developers"
+PROJECT = "Keylime Documentation"
+COPYRIGHT = "2022, Keylime Developers"
+AUTHOR = "Keylime Developers"
 
 # The short X.Y version
-version = ""
+VERSION = ""
 # The full version, including alpha/beta/rc tags
-release = "7.6.0"
+RELEASE = "7.6.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -57,18 +57,18 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+# SOURCE_SUFFIX = ['.rst', '.md']
+SOURCE_SUFFIX = ".rst"
 
 # The master toctree document.
-master_doc = "index"
+MASTER_DOC = "index"
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+LANGUAGE = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -76,7 +76,7 @@ language = None
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+PYGMENTS_STYLE = None
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -84,8 +84,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+# HTML_THEME = 'alabaster'
+HTML_THEME = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
@@ -111,7 +111,7 @@ html_static_path = ["_static"]
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = "KeylimeDocumentationdoc"
+HTMLHELP_BASENAME = "KeylimeDocumentationdoc"
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -135,7 +135,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, "KeylimeDocumentation.tex", "Keylime Documentation Documentation", "Keylime Developers", "manual"),
+    (MASTER_DOC, "KeylimeDocumentation.tex", "Keylime Documentation Documentation", "Keylime Developers", "manual"),
 ]
 
 
@@ -143,7 +143,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "keylimedocumentation", "Keylime Documentation Documentation", [author], 1)]
+man_pages = [(MASTER_DOC, "keylimedocumentation", "Keylime Documentation Documentation", [AUTHOR], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------------
@@ -153,10 +153,10 @@ man_pages = [(master_doc, "keylimedocumentation", "Keylime Documentation Documen
 #  dir menu entry, description, category)
 texinfo_documents = [
     (
-        master_doc,
+        MASTER_DOC,
         "KeylimeDocumentation",
         "Keylime Documentation Documentation",
-        author,
+        AUTHOR,
         "KeylimeDocumentation",
         "One line description of project.",
         "Miscellaneous",
@@ -167,7 +167,7 @@ texinfo_documents = [
 # -- Options for Epub output -------------------------------------------------
 
 # Bibliographic Dublin Core info.
-epub_title = project
+EPUB_TITLE = PROJECT
 
 # The unique identifier of the text. This can be a ISBN number
 # or the project homepage.

demo/agent_monitor/tenant_agent_monitor.py
@@ -33,7 +33,7 @@ sys.path.insert(0, "../../keylime/")
 
 logger = keylime_logging.init_logging("agent_monitor")
 
-initscript = None
+INIT_SCRIPT = None
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -125,17 +125,17 @@ class AgentsHandler(BaseHandler):
                     ssl.match_hostname(client_cert, agent_id)
 
                     # Execute specified script if all is well
-                    global initscript
-                    if initscript is not None and initscript != "":
+                    global INIT_SCRIPT
+                    if INIT_SCRIPT is not None and INIT_SCRIPT != "":
 
                         def initthread():
                             import subprocess
 
-                            logger.debug("Executing specified script: %s" % initscript)
+                            logger.debug("Executing specified script: %s" % INIT_SCRIPT)
                             env = os.environ.copy()
                             env["AGENT_UUID"] = agent_id
                             proc = subprocess.Popen(
-                                ["/bin/sh", initscript],
+                                ["/bin/sh", INIT_SCRIPT],
                                 env=env,
                                 shell=False,
                                 stdout=subprocess.PIPE,
@@ -148,18 +148,18 @@ class AgentsHandler(BaseHandler):
                                     break
                                 logger.debug("init-output: %s" % line.strip())
 
-                        t = threading.Thread(target=initthread)
-                        t.start()
+                        thread = threading.Thread(target=initthread)
+                        thread.start()
 
                     keylime.web_util.echo_json_response(self, 200, "Success", json_body)
                     logger.info("POST returning 200 response for Agent Monitor connection as " + agent_id)
             else:
                 keylime.web_util.echo_json_response(self, 400, "uri not supported")
                 logger.warning("POST returning 400 response. uri not supported")
-        except Exception as e:
-            keylime.web_util.echo_json_response(self, 400, "Exception error: %s" % e)
-            logger.warning("POST returning 400 response. Exception error: %s" % e)
-            logger.exception(e)
+        except Exception as err:
+            keylime.web_util.echo_json_response(self, 400, "Exception error: %s" % err)
+            logger.warning("POST returning 400 response. Exception error: %s" % err)
+            logger.exception(err)
 
     def put(self):
         """This method handles the PUT requests to add agents to the Agent Monitor.
@@ -240,8 +240,8 @@ def main(argv=sys.argv):
         args.ca_dir = common.CA_WORK_DIR
 
     # Make initscript available to tornado callback
-    global initscript
-    initscript = args.script
+    global INIT_SCRIPT
+    INIT_SCRIPT = args.script
 
     logger.info("Starting Agent Monitor (tornado) on port " + args.port + ", use <Ctrl-C> to stop")
 

keylime/agentstates.py
@@ -218,12 +218,12 @@ class AgentAttestStates:
         """Get an agent's state given its id"""
 
         with self.map_lock:
-            agentAttestState = self.map.get(agent_id)
-            if not agentAttestState:
-                agentAttestState = AgentAttestState(agent_id)
-                self.map[agent_id] = agentAttestState
+            agent_attest_state = self.map.get(agent_id)
+            if not agent_attest_state:
+                agent_attest_state = AgentAttestState(agent_id)
+                self.map[agent_id] = agent_attest_state
 
-        return agentAttestState
+        return agent_attest_state
 
     def delete_by_agent_id(self, agent_id: str) -> None:
         """Delete an agent's state given its id"""

keylime/api_version.py
@@ -16,9 +16,9 @@ def current_version() -> str:
     return CURRENT_VERSION
 
 
-def latest_minor_version(v: VersionType) -> str:
+def latest_minor_version(version_type: VersionType) -> str:
     try:
-        v_obj = version.parse(str(v))
+        v_obj = version.parse(str(version_type))
     except version.InvalidVersion:
         return "0"
     if not isinstance(v_obj, version.Version):
@@ -34,29 +34,29 @@ def all_versions() -> List[str]:
     return VERSIONS.copy()
 
 
-def is_supported_version(v: VersionType) -> bool:
+def is_supported_version(version_type: VersionType) -> bool:
     try:
-        v_obj = version.parse(str(v))
+        v_obj = version.parse(str(version_type))
     except version.InvalidVersion:
         return False
     return v_obj.base_version in VERSIONS
 
 
-def is_deprecated_version(v: VersionType) -> bool:
+def is_deprecated_version(version_type: VersionType) -> bool:
     try:
-        v_obj = version.parse(str(v))
+        v_obj = version.parse(str(version_type))
     except version.InvalidVersion:
         return False
-    return is_supported_version(v) and v_obj.base_version in DEPRECATED_VERSIONS
+    return is_supported_version(version_type) and v_obj.base_version in DEPRECATED_VERSIONS
 
 
-def normalize_version(v: VersionType) -> str:
-    v = str(v)
-    v = v.strip("/")
+def normalize_version(version_type: VersionType) -> str:
+    version_type = str(version_type)
+    version_type = version_type.strip("/")
     try:
-        base_version = version.parse(v).base_version
+        base_version = version.parse(version_type).base_version
     except version.InvalidVersion:
-        return v
+        return version_type
     # if the base version is a single number, get the latest minor version
     if "." not in base_version:
         latest_minor = latest_minor_version(base_version)
@@ -65,18 +65,18 @@ def normalize_version(v: VersionType) -> str:
     return base_version
 
 
-def major(v: VersionType) -> int:
+def major(version_type: VersionType) -> int:
     try:
-        v_obj = version.parse(str(v))
+        v_obj = version.parse(str(version_type))
     except version.InvalidVersion:
         return 0
     assert isinstance(v_obj, version.Version)
     return v_obj.major
 
 
-def minor(v: VersionType) -> int:
+def minor(version_type: VersionType) -> int:
     try:
-        v_obj = version.parse(str(v))
+        v_obj = version.parse(str(version_type))
     except version.InvalidVersion:
         return 0
     assert isinstance(v_obj, version.Version)
@@ -93,6 +93,6 @@ def log_api_versions(logger: Logger) -> None:
         logger.info("Deprecated API versions (soon to be removed): " + ", ".join(DEPRECATED_VERSIONS))
 
 
-def validate_version(v: str) -> bool:
+def validate_version(version_type: str) -> bool:
     pattern = re.compile(r"\d.\d")
-    return re.fullmatch(pattern, v) is not None
+    return re.fullmatch(pattern, version_type) is not None

keylime/cert_utils.py
@@ -35,8 +35,8 @@ def x509_der_cert(der_cert_data: bytes) -> Certificate:
     """
     try:
         return x509.load_der_x509_certificate(data=der_cert_data, backend=default_backend())
-    except Exception as e:
-        logger.warning("Failed to parse DER data with python-cryptography: %s", e)
+    except Exception as err:
+        logger.warning("Failed to parse DER data with python-cryptography: %s", err)
         pyasn1_cert = decoder.decode(der_cert_data, asn1Spec=rfc2459.Certificate())[0]
         return x509.load_der_x509_certificate(data=encoder.encode(pyasn1_cert), backend=default_backend())
 
@@ -49,8 +49,8 @@ def x509_pem_cert(pem_cert_data: str) -> Certificate:
     """
     try:
         return x509.load_pem_x509_certificate(data=pem_cert_data.encode("utf-8"), backend=default_backend())
-    except Exception as e:
-        logger.warning("Failed to parse PEM data with python-cryptography: %s", e)
+    except Exception as err:
+        logger.warning("Failed to parse PEM data with python-cryptography: %s", err)
         # Let's read the DER bytes from the base-64 PEM.
         der_data = pem.readPemFromFile(io.StringIO(pem_cert_data))
         # Now we can load it as we do in x509_der_cert().
@@ -66,8 +66,8 @@ def verify_ek(ekcert: bytes, tpm_cert_store: str) -> bool:
     """
     try:
         trusted_certs = tpm_ek_ca.cert_loader(tpm_cert_store)
-    except Exception as e:
-        logger.warning("Error loading trusted certificates from the TPM cert store: %s", e)
+    except Exception as err:
+        logger.warning("Error loading trusted certificates from the TPM cert store: %s", err)
         return False
 
     try:
@@ -102,9 +102,9 @@ def verify_ek(ekcert: bytes, tpm_cert_store: str) -> bool:
 
             logger.debug("EK cert matched cert: %s", cert_file)
             return True
-    except Exception as e:
+    except Exception as err:
         # Log the exception so we don't lose the raw message
-        logger.exception(e)
+        logger.exception(err)
         raise Exception("Error processing ek/ekcert. Does this TPM have a valid EK?").with_traceback(sys.exc_info()[2])
 
     logger.error("No Root CA matched EK Certificate")
@@ -142,7 +142,7 @@ def verify_ek_script(script: Optional[str], env: Optional[Dict[str, str]], cwd: 
         logger.debug("External check script successfully to validated EK")
         if proc.stdout is not None:
             logger.info("ek_check output: %s", proc.stdout.decode("utf-8"))
-    except subprocess.CalledProcessError as e:
-        logger.error("Error while trying to run external check script to validate EK: %s", e)
+    except subprocess.CalledProcessError as err:
+        logger.error("Error while trying to run external check script to validate EK: %s", err)
         return False
     return True

keylime/db/keylime_db.py
@@ -103,9 +103,9 @@ class SessionManager:
         To use: session = self.make_session(engine)
         """
         self.engine = engine
-        MySession = scoped_session(sessionmaker())
+        my_session = scoped_session(sessionmaker())
         try:
-            MySession.configure(bind=self.engine)  # type: ignore
-        except SQLAlchemyError as e:
-            logger.error("Error creating SQL session manager %s", e)
-        return cast(Session, MySession())
+            my_session.configure(bind=self.engine)  # type: ignore
+        except SQLAlchemyError as err:
+            logger.error("Error creating SQL session manager %s", err)
+        return cast(Session, my_session())

commit 07daf8caa25a3295e7ae075689fe0a903b258144

docs: fix conf.py
8b5decd accidentally also the variables used for Sphinx configuration.

Signed-off-by: Thore Sommer <mail@thson.de>

docs/conf.py
@@ -1,3 +1,4 @@
+# pylint: disable=invalid-name
 # -*- coding: utf-8 -*-
 #
 # Configuration file for the Sphinx documentation builder.
@@ -19,14 +20,14 @@
 
 # -- Project information -----------------------------------------------------
 
-PROJECT = "Keylime Documentation"
-COPYRIGHT = "2022, Keylime Developers"
-AUTHOR = "Keylime Developers"
+project = "Keylime Documentation"
+copyright = "2023, Keylime Developers"
+author = "Keylime Developers"
 
 # The short X.Y version
-VERSION = ""
+version = ""
 # The full version, including alpha/beta/rc tags
-RELEASE = "7.8.0"
+release = "7.8.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -50,6 +51,7 @@ extensions = [
     "recommonmark",
     "notfound.extension",
     "sphinxcontrib.bibtex",
+    "sphinx_rtd_theme",
 ]
 bibtex_bibfiles = ["publications.bib"]
 
@@ -60,17 +62,17 @@ templates_path = ["_templates"]
 # You can specify multiple suffix as a list of string:
 #
 # SOURCE_SUFFIX = ['.rst', '.md']
-SOURCE_SUFFIX = ".rst"
+source_suffix = ".rst"
 
 # The master toctree document.
-MASTER_DOC = "index"
+master_doc = "index"
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-LANGUAGE = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -78,7 +80,7 @@ LANGUAGE = None
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-PYGMENTS_STYLE = None
+pygments_style = None
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -87,7 +89,7 @@ PYGMENTS_STYLE = None
 # a list of builtin themes.
 #
 # HTML_THEME = 'alabaster'
-HTML_THEME = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
@@ -113,7 +115,7 @@ html_static_path = ["_static"]
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-HTMLHELP_BASENAME = "KeylimeDocumentationdoc"
+htmlhelp_basename = "KeylimeDocumentationdoc"
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -137,7 +139,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (MASTER_DOC, "KeylimeDocumentation.tex", "Keylime Documentation Documentation", "Keylime Developers", "manual"),
+    (master_doc, "KeylimeDocumentation.tex", "Keylime Documentation Documentation", "Keylime Developers", "manual"),
 ]
 
 
@@ -145,7 +147,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(MASTER_DOC, "keylimedocumentation", "Keylime Documentation Documentation", [AUTHOR], 1)]
+man_pages = [(master_doc, "keylimedocumentation", "Keylime Documentation Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------------
@@ -155,10 +157,10 @@ man_pages = [(MASTER_DOC, "keylimedocumentation", "Keylime Documentation Documen
 #  dir menu entry, description, category)
 texinfo_documents = [
     (
-        MASTER_DOC,
+        master_doc,
         "KeylimeDocumentation",
         "Keylime Documentation Documentation",
-        AUTHOR,
+        author,
         "KeylimeDocumentation",
         "One line description of project.",
         "Miscellaneous",
@@ -169,7 +171,7 @@ texinfo_documents = [
 # -- Options for Epub output -------------------------------------------------
 
 # Bibliographic Dublin Core info.
-EPUB_TITLE = PROJECT
+epub_title = project
 
 # The unique identifier of the text. This can be a ISBN number
 # or the project homepage.