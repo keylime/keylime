2b231b7ea32d33fd465eebb44f773379da4e5780
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2024-07-29 17:56:31 +0200

    docker: Build base image separately
    Add a workflow to automatically create pull requests updating the Fedora
    image used to build the base image.
    
    Build the base image separately when the Dockerfile template is updated.
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- docker/release/build_locally.sh
+++ docker/release/build_locally.sh
@@ -15,7 +15,17 @@ if [ -z "${IMAGE_BASE}" ]; then
     IMAGE_BASE="${REGISTRY}/keylime"
 fi
 
+FEDORA_IMAGE="${REGISTRY}/fedora/fedora"
+FEDORA_DIGEST="$(skopeo inspect docker://${FEDORA_IMAGE}:latest | jq '.Digest')"
+
+# Prepare base image Dockerfile
+sed -i "s#\(FROM \)[^ ]*#\1${FEDORA_IMAGE}@${FEDORA_DIGEST}#" base/Dockerfile.in
+sed "s#_version_#${VERSION}#" base/Dockerfile.in > base/Dockerfile
+
+# Prepare other components Dockerfile
 ./generate-files.sh ${VERSION}
+
+# Build images
 for part in base registrar verifier tenant; do
   docker buildx build -t keylime_${part}:${VERSION} -f "${part}/Dockerfile" --security-opt label=disable --progress plain ${@:3} "$KEYLIME_DIR"
   rm -f ${part}/Dockerfile
--- docker/release/generate-files.sh
+++ docker/release/generate-files.sh
@@ -7,11 +7,17 @@
 
 VERSION=${1:-latest}
 SOURCE=${2:-""}
+DIGEST=${3:-""}
 
 # Docker only allows lower case repositories
 SOURCE=$(echo -n ${SOURCE} | tr '[:upper:]' '[:lower:]')
-for part in base registrar verifier tenant; do
+for part in registrar verifier tenant; do
   echo "Generating ${part}"
   sed "s#_version_#${VERSION}#" "${part}/Dockerfile.in" > ${part}/Dockerfile
   sed -i "s#_source_#${SOURCE}#" ${part}/Dockerfile
+  if [ -n "$DIGEST" ]; then
+    sed -i "s#_digest_#@${DIGEST}#" ${part}/Dockerfile
+  else
+    sed -i "s#_digest_#:${VERSION}#" ${part}/Dockerfile
+  fi
 done
