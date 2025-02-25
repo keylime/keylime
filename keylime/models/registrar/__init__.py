34af08f0af8be3669742de1d75a9d0615e48fd71
Jean Snyman <git@jsnyman.com>
2025-02-25 14:19:15 +0000

    Remove changes related to the trust decisions webhook
    Signed-off-by: Jean Snyman <git@jsnyman.com>

--- keylime/models/registrar/__init__.py
+++ keylime/models/registrar/__init__.py
@@ -1,13 +1,3 @@
-from keylime.models.registrar.identity_decision import Identity, IdentityBinding, IdentityDecision, IdentityTrust
-from keylime.models.registrar.identity_decision_update import IdentityDecisionUpdate, IdentityOverride
 from keylime.models.registrar.registrar_agent import RegistrarAgent
 
-__all__ = [
-    "RegistrarAgent",
-    "IdentityDecision",
-    "Identity",
-    "IdentityTrust",
-    "IdentityBinding",
-    "IdentityDecisionUpdate",
-    "IdentityOverride",
-]
+__all__ = ["RegistrarAgent"]
