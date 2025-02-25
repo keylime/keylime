6cb6bb2aba92908264edf45682920f83eeefd386
Jean Snyman <git@jsnyman.com>
2025-02-25 14:28:02 +0000

    Replace Base64Bytes with more versatile Binary data type
    Signed-off-by: Jean Snyman <git@jsnyman.com>

--- keylime/models/base/types/__init__.py
+++ keylime/models/base/types/__init__.py
@@ -1,5 +1,4 @@
 from keylime.models.base.types.binary import Binary
-from keylime.models.base.types.base64_bytes import Base64Bytes
 from keylime.models.base.types.certificate import Certificate
 from keylime.models.base.types.dictionary import Dictionary
 from keylime.models.base.types.list import List
