b5eb4c04e24e536df872fefd5bb2549a44ce6c25
Jean Snyman <git@jsnyman.com>
2025-02-26 00:00:45 +0000

    Prevent verifier.conf being read on registrar start
    Signed-off-by: Jean Snyman <git@jsnyman.com>

--- keylime/web/verifier/server_info_controller.py
+++ keylime/web/verifier/server_info_controller.py
@@ -1,10 +1,11 @@
 from keylime.web.base import Controller
-from keylime import cloud_verifier_tornado as v2
 from keylime import config, api_version
 
 
 class ServerInfoController(Controller):
     def _new_v2_main_handler(self):
+        from keylime import cloud_verifier_tornado as v2
+
         tornado_app = self.action_handler.application
         tornado_req = self.action_handler.request
         return v2.MainHandler(tornado_app, tornado_req, override=self.action_handler)
