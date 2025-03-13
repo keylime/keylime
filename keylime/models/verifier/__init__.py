89cd5b30bf704c6ffacf54533b6e1bbdcde14e97
Jean Snyman <git@jsnyman.com>
2025-03-13 14:38:04 +0000

    Add models to reflect new JSON format
    Signed-off-by: Jean Snyman <git@jsnyman.com>

--- keylime/models/verifier/__init__.py
+++ keylime/models/verifier/__init__.py
@@ -2,3 +2,5 @@ from keylime.models.verifier.verifier_agent import VerifierAgent
 from keylime.models.verifier.ima_policy import IMAPolicy
 from keylime.models.verifier.mb_policy import MBPolicy
 from keylime.models.verifier.push_attestation import PushAttestation
+from keylime.models.verifier.attestation import Attestation
+from keylime.models.verifier.evidence import EvidenceItem
