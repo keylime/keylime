d0e51cf516e10e6ab13094ea823cd879e71af356
Sergio Arroutbi <sarroutb@redhat.com>
2025-06-10 17:16:30 +0200

    CI: Enable CONTAINER_ENGINE to allow other engines
    Minimum change to allow CONTAINER_ENGINE
    environment variable to be set with a
    different container engine (i.e.podman).
    It will also check if podman exists and
    docker don't, using it in that case
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- .ci/run_local.sh
+++ .ci/run_local.sh
@@ -5,23 +5,37 @@
 # The default value is one directory above where this script is located.
 REPO=${KEYLIME_REPO_PATH:-${1:-$(realpath "$(dirname "$(readlink -f "$0")")/../")}}
 
+# You can specify the container engine to use with the CONTAINER_ENGINE
+# environment variable. The default to be used will be 'docker',
+# or 'podman' if it exists and 'docker' don't
+if [ -z "$CONTAINER_ENGINE" ]; then
+    if command -v docker &> /dev/null; then
+        CONTAINER_ENGINE="docker"
+    elif command -v podman &> /dev/null; then
+        CONTAINER_ENGINE="podman"
+    else
+        echo "ERROR: No container engine specified, and could not find 'docker' or 'podman' in PATH." >&2
+        exit 1
+    fi
+fi
+
 # keylime images
 tpmimage="quay.io/keylime/keylime-ci"
 tpmtag="latest"
 
 echo -e "Fetching Keylime CI container"
 
-docker pull ${tpmimage}:${tpmtag}
+${CONTAINER_ENGINE} pull ${tpmimage}:${tpmtag}
 
 echo -e "Running Keylime's test suite"
 
 container_id=$(mktemp)
-docker run --detach --user root --env KEYLIME_TEST='true' --env RUST_TEST=1\
+${CONTAINER_ENGINE} run --detach --user root --env KEYLIME_TEST='true' --env RUST_TEST=1\
     -v $REPO:/root/keylime:Z \
     --mount type=tmpfs,destination=/var/lib/keylime/,tmpfs-mode=1770 \
     -it ${tpmimage}:${tpmtag} >> ${container_id}
 # run the Keylime test suite
-docker exec -u root -it --tty "$(cat ${container_id})" \
+${CONTAINER_ENGINE} exec -u root -it --tty "$(cat ${container_id})" \
     /bin/bash /root/keylime/.ci/test_wrapper.sh
-docker stop "$(cat ${container_id})"
-docker rm "$(cat ${container_id})"
+${CONTAINER_ENGINE} stop "$(cat ${container_id})"
+${CONTAINER_ENGINE} rm "$(cat ${container_id})"
