8a946fedfedff5a06b8922c891b14559f38d516b
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2026-02-06 22:52:54 +0100

    Bump version to 7.12.2
    Prepare for the 7.12.2 release
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Keylime Developers"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "7.12.1"
+release = "7.12.2"
 
 
 # -- General configuration ---------------------------------------------------
